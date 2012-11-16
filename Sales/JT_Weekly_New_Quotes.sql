c9e42a2f98d668a852c4a4b4a05de79c9a03e1d7
James Tuttle <jamest@tasupply.com>
2012-11-16 10:06:02 -0800

    Added the files for text created the week end and start variables added by seq# and text# to only get one record per line

--- Sales/JT_Weekly_New_Quotes.sql
+++ Sales/JT_Weekly_New_Quotes.sql
@@ -13,16 +13,56 @@
 --------------------------------------------------------------------------------
 
 
--- CREATE PROC JT_Weekly_New_Quotes AS
-BEGIN
+ALTER PROC JT_Weekly_New_Quotes 
+ @BeginDate varchar(10)
+,@EndDate varchar(10) 
+AS
+BEGIN 
+
+	-- Last Saturday's date
+	SET @BeginDate = CONVERT(VARCHAR(10),dateadd(dd,-6,datediff(dd,0,getdate())),101)	
+	-- Today's date
+	SET @EndDate = CONVERT(VARCHAR(10),GETDATE(),101)	
+ 
+DECLARE @SQL AS varchar(4000)
+SET @SQL ='
+
 	SELECT *
-	FROM OPENQUERY (GSFL2K,
-		'SELECT 
-		
-		FROM
-		WHERE
-		')
-
-
-
+	FROM OPENQUERY(GSFL2K,
+		''SELECT ohco		AS Company#
+				,ohloc		AS Quote_Loc
+				,ohord#		AS Quote#
+				,ohcont		AS Cust_Contact
+				,olitem		AS Product
+				,olpric		AS Price
+				,olqord		AS Quantity
+				,ohodat		AS Orig_Qt_Date
+				,ohddat		AS Exp_Date
+				,ohpo#		AS PO#
+				,otcmt1		AS Sidemark 
+		 FROM qshead qh 
+		 JOIN qsline ql ON 
+			( qh.ohco = ql.olco
+				AND qh.ohloc = ql.olloc
+				AND qh.ohord# = ql.olord#
+				AND qh.ohrel# = ql.olrel#
+				AND qh.ohcust = ql.olcust)
+		 JOIN qstext qt ON 
+ 			( qh.ohco = qt.otco
+				AND qh.ohloc = qt.otloc
+				AND qh.ohord# = qt.otord#
+				AND qh.ohrel# = qt.otrel#
+				AND qh.ohcust = qt.otcust)
+		WHERE qh.ohodat >=  ' + '''' + '''' +@BeginDate + '''' + ''''+ '   
+			AND qh.ohodat <=  ' + '''' + '''' + @EndDate + '''' + ''''+ '
+			AND qt.ottseq = 1
+			AND qt.otseq# = 0
+		ORDER BY qh.ohco
+				,qh.ohloc
+				,qh.ohord#
+		'')
+	'
+	EXEC (@SQL)
 END
+
+--	JT_Weekly_New_Quotes 11102012, 11162012
