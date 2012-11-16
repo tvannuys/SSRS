17c6a256fd41b12d990e5822d50b0d9ea5cc0caf
James Tuttle <jamest@tasupply.com>
2012-11-16 08:49:31 -0800

    Initial file upload

---
+++ Sales/JT_Weekly_New_Quotes.sql
@@ -0,0 +1,27 @@
+
+--------------------------------------------------------------------------------
+--	SR# 4980 - 
+--	James Tuttle
+--	11/16/2012
+--
+--	PURPOSE: A subscription that Email out on Friday at 5PM
+--			of that week's quotes written.
+--
+--
+--
+--
+--------------------------------------------------------------------------------
+
+
+-- CREATE PROC JT_Weekly_New_Quotes AS
+BEGIN
+	SELECT *
+	FROM OPENQUERY (GSFL2K,
+		'SELECT 
+		
+		FROM
+		WHERE
+		')
+
+
+END
