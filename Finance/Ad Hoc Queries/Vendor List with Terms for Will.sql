712f7ed31772616238863109f0431040e31671c6
Thomas Van Nuys <tvannuys@tasupply.com>
2012-11-30 13:26:16 -0800

    query for will to get a vendor list with terms

---
+++ Finance/Ad Hoc Queries/Vendor List with Terms for Will.sql
@@ -0,0 +1,21 @@
+select * from openquery(gsfl2k,'
+select 
+vendmast.vmvend,
+vmname,
+vmterm,
+pudesc,
+vmgl,
+vsys$,
+vsyc$
+
+
+from vendmast
+left join poterms on vendmast.vmterm = poterms.puterm
+left join vendstat on vendmast.vmvend = vendstat.vsvend
+
+and vsloc = 98
+and vsco = 2
+
+
+
+')
