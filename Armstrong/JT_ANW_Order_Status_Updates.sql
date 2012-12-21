010be5ad4ac8c7f11c40e478096354d25429a901
James Tuttle <jamest@tasupply.com>
2012-12-21 11:41:32 -0800

    Not to include orders in shipped status

--- Armstrong/JT_ANW_Order_Status_Updates.sql
+++ Armstrong/JT_ANW_Order_Status_Updates.sql
@@ -12,31 +12,32 @@
 --
 ----------------------------------------------------------------------
  --ALTER PROC JT_ANW_Order_Status_Updates AS
-SELECT *
-FROM OPENQUERY(GSFL2K,
-	'SELECT ohord# AS Order#
-			,ohrel# AS Rel#
-			,ohvia AS Via
-			,ortrt AS Route
-			,rtdesc AS Description
-			,MONTH(ortsdt) || ''/'' || DAY(ortsdt) || ''/'' || YEAR(ortsdt) AS Ship_Date
-			,MONTH(ortadt) || ''/'' || DAY(ortadt) || ''/'' || YEAR(ortadt) AS Arrive_Date
-	FROM oohead oh
-/*	JOIN ooline ol ON (oh.ohco = ol.olco
-							AND oh.ohloc = ol.olloc
-							AND oh.ohord# = ol.olord#
-							AND oh.ohrel# = ol.olrel#
-							AND oh.ohcust = ol.olcust)	*/
-	JOIN ooroute rt ON (oh.ohco = rt.ortco
-							AND oh.ohloc = rt.ortloc
-							AND oh.ohord# = rt.ortord
-							AND oh.ohrel# = rt.ortrel
-							AND oh.ohcust = rt.ortcus)
-	JOIN route rte ON rt.ortrt = rte.rtrout
-	WHERE oh.ohbil# = ''4100000''
-		/*	AND ol.olinvu	*/
-		/* AND oh.ohord# = 279943	*/
-	ORDER BY oh.ohord#
-			
-	')
+ BEGIN
+	SELECT DISTINCT *
+	FROM OPENQUERY(GSFL2K,
+		'SELECT ohord# AS Order#
+				,ohrel# AS Rel#
+				,ohvia AS Via
+				,ortrt AS Route
+				,rtdesc AS Description
+				,MONTH(ortsdt) || ''/'' || DAY(ortsdt) || ''/'' || YEAR(ortsdt) AS Ship_Date
+				,MONTH(ortadt) || ''/'' || DAY(ortadt) || ''/'' || YEAR(ortadt) AS Arrive_Date
+		FROM oohead oh
+		JOIN ooline ol ON (oh.ohco = ol.olco
+								AND oh.ohloc = ol.olloc
+								AND oh.ohord# = ol.olord#
+								AND oh.ohrel# = ol.olrel#
+								AND oh.ohcust = ol.olcust)	
+		JOIN ooroute rt ON (oh.ohco = rt.ortco
+								AND oh.ohloc = rt.ortloc
+								AND oh.ohord# = rt.ortord
+								AND oh.ohrel# = rt.ortrel
+								AND oh.ohcust = rt.ortcus)
+		JOIN route rte ON rt.ortrt = rte.rtrout
+		WHERE oh.ohbil# = ''4100000''
+			AND ol.olinvu != ''T''
+		ORDER BY oh.ohord#
+				
+		')
+END
 	
