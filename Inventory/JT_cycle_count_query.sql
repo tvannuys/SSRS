df48e89b521c5ab447f27c355806ff3a930a12d2
James Tuttle <jamest@tasupply.com>
2013-01-09 08:02:45 -0800

    Initial file upload

--- Inventory/JT_cycle_count_query.sql
+++ Inventory/JT_cycle_count_query.sql
@@ -1,8 +1,16 @@
 
-
-
-
-
+/*********************************************************
+**														**
+** SR# nnnn												**
+** Programmer: James Tuttle		Date: 06/05/2012		**
+** ---------------------------------------------------- **
+** Purpose:		Count the tranaction code of 'I' and	**
+**		not the recount code of 'Y'. This will give		**
+**		the lines cycle counted for the given time		**
+**		period and co/loc.								**
+**														**
+**														**
+**********************************************************/
 
 ALTER PROC JT_cycle_count_query
 	@StartDate VARCHAR(10)
