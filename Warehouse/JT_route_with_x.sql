7451628d5efec47d6e3dcec46c3bce7fa9da0267
James Tuttle <jamest@tasupply.com>
2013-01-16 11:13:17 -0800

    SR# 6936 Add Order Type field to report

--- Warehouse/JT_route_with_x.sql
+++ Warehouse/JT_route_with_x.sql
@@ -1,5 +1,5 @@
 
---CREATE PROC JT_route_with_x AS
+
 
 /* -------------------------------------------------*	 
 *	James Tuttle									*
@@ -9,15 +9,19 @@
 * look at open orders for a route LIKE aaaaX		*
 *---------------------------------------------------*/												
 													
+ALTER PROC JT_route_with_x AS
 
-
-
-SELECT *
-FROM OPENQUERY (GSFL2K, 'SELECT ohco,
-						ohloc,
-						ohord#,
-						ohrel#,
-						ohrout
-					FROM oohead
-					WHERE ohrout LIKE ''%X%''
-			')
+BEGIN
+	SELECT *
+	FROM OPENQUERY (GSFL2K, 'SELECT ohco
+							,ohloc
+							,ohotyp
+							,ohord#
+							,ohrel#
+							,ohrout
+						FROM oohead
+						WHERE ohrout LIKE ''%X%''
+						ORDER BY ohotyp DESC
+				')
+END
+			
