b5cd4511bfdb1c855083b33880d33aefc34dadca
Thomas Van Nuys <tvannuys@tasupply.com>
2013-01-17 14:49:01 -0800

    Initial load

---
+++ jobs/spGLBalanceExtract.sql
@@ -0,0 +1,58 @@
+USE [GartmanReport]
+GO
+
+/****** Object:  StoredProcedure [dbo].[GLBalanceExtract]    Script Date: 01/17/2013 14:45:13 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+CREATE proc [dbo].[GLBalanceExtract] as
+
+drop table glBalance
+
+select * 
+into GLBalance
+from openquery (GSFL2K,'
+select gbco,
+gbloc,
+lcname,
+gbgl#,
+gldesc,
+gbyr,
+gbopbl,
+gba01,
+gba02,
+gba03,
+gba04,
+gba05,
+gba06,
+gba07,
+gba08,
+gba09,
+gba10,
+gba11,
+gba12
+
+
+from GLBAL 
+left join location on (glbal.gbco = location.lcco
+	and glbal.gbloc = location.lcloc)
+left join glmast on (glbal.gbco = glmast.glco
+	and glbal.gbgl# = glmast.glgl#)
+
+where gbyr in (2010,2011,2012)
+')
+
+
+
+
+
+
+
+GO
+
+
