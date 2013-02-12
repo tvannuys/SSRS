ddd158e58c2fa4bf6ff8ff8dfb87b45a563c82cd
James Tuttle <jamest@tasupply.com>
2013-02-12 09:39:55 -0800

    Updated structure

--- Purchasing/RollGoodsDataSet.sql
+++ Purchasing/RollGoodsDataSet.sql
@@ -10,15 +10,78 @@
 **			with bin location, vendor, item, qty sold  	**
 **			last 4 months, dollars sold last 4 months	**
 **														**
+** Name: RollGoodsDataSet								**
+**														**
 **********************************************************/
 
-CREATE PROC RollGoodsDataSet AS
-BEGIN
- SELECT *
- FROM OPENQUERY(GSFL2K,	
-	'SELECT 
-		,
-		,
-	FROM
+--========================================================
+--					D A T A S E T						--
+--========================================================
+ 
+SELECT idco					AS Co
+		,idloc				AS Loc
+		,iditem				AS Item
+		,imdesc				AS [Description]
+		,idserl				AS Serail#
+		,iddylt				AS DyeLot
+		,idky				AS Tag#
+		,idbin				AS Bin
+		,(idqoh - idqoo)	AS Feet_Avbl
+		,idvend				AS Vender#
+		,vmname				AS Vendor_Name
+		,Feet_Sold_Last_4_Months
+		,Total_$_Last_4_Months
+				
+FROM OPENQUERY(GSFL2K,'
+	SELECT idco
+			,idloc
+			,iditem
+			,imdesc
+			,idserl
+			,iddylt
+			,idky
+			,idbin
+			,idqoh
+			,idqoo
+			,idvend
+			,vmname
+/*------------------ Get the last four months qty (feet) sold ------------------------------------------------------ */
+/*-------------------------------------------------------------------------------------------------------------------*/
+			,(SELECT SUM(slqshp) FROM shline sl2 WHERE sl2.sldate >= CURRENT_DATE -120 DAYS
+													AND sl2.slitem = id.iditem) AS Feet_Sold_Last_4_Months
+/*------------------ Get the last four months dollars sold --------------------------------------------------------- */
+/*-------------------------------------------------------------------------------------------------------------------*/
+			,(SELECT SUM(sleprc) FROM shline sl2 WHERE sl2.sldate >= CURRENT_DATE -120 DAYS
+													AND sl2.slitem = id.iditem) AS Total_$_Last_4_Months
+					
+	FROM ITEMDETL id
+	LEFT JOIN ITEMMAST im ON im.imitem = id.iditem
+	LEFT JOIN VENDMAST vm ON vm.vmvend = id.idvend
+	LEFT JOIN SHLINE sl ON sl.slitem = id.iditem
+	LEFT JOIN BINLOC bl ON bl.blbin = id.idbin
+	
+	WHERE sl.sldate >=  ''02/01/2013''
+		AND id.idfcrg = ''Y''
+		AND id.idqoh > 0.0
+		AND bl.blgrp != ''XXXXX''
 	')
-END
+GROUP BY idco
+		,idLoc
+		,iditem
+		,imdesc
+		,idserl
+		,iddylt
+		,idky
+		,idbin
+		,idvend
+		,vmname
+		,idqoh
+		,idqoo
+		,Feet_Sold_Last_4_Months
+		,Total_$_Last_4_Months
+			
+HAVING (idqoh - idqoo) <= 30.00
+	AND (idqoh - idqoo) > 0.00
+-----------------------------------------------------------
+
+-- 	
