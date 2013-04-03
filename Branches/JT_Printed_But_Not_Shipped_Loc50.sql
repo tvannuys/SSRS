199322b6e9c1fdd32edb72e046be590b7b8fdb56
James Tuttle <jamest@tasupply.com>
2013-04-03 08:45:10 -0700

    Changed the date format to MM/DD/YYYY

--- Branches/JT_Printed_But_Not_Shipped_Loc50.sql
+++ Branches/JT_Printed_But_Not_Shipped_Loc50.sql
@@ -1,16 +1,6 @@
 USE [GartmanReport]
 GO
 
-/****** Object:  StoredProcedure [dbo].[JT_Printed_But_Not_Shipped_Loc50]    Script Date: 04/03/2013 07:40:05 ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[JT_Printed_But_Not_Shipped_Loc50]') AND type in (N'P', N'PC'))
-BEGIN
-EXEC dbo.sp_executesql @statement = N'
 
 
 ALTER PROC [dbo].[JT_Printed_But_Not_Shipped_Loc50] AS
@@ -30,12 +20,12 @@ ALTER PROC [dbo].[JT_Printed_But_Not_Shipped_Loc50] AS
 /* Query to look at orders printed but not shipped for Will Call */
 
 SELECT DISTINCT *
-	FROM OPENQUERY (GSFL2K, ''SELECT cmname AS CustName
+	FROM OPENQUERY (GSFL2K, 'SELECT cmname AS CustName
 									,ohprco AS CO
 									,ohprlo AS Loc
 									,ohord# AS Order
 									,ohrel# AS Release
-									,ohdtp AS Date_Printed
+									,MONTH(ohdtp) || ''/'' || DAY(ohdtp) || ''/'' || YEAR(ohdtp) AS Date_Printed
 									,ohviac AS Via_Code
 									,ohvia AS Via_Description
 								,olinvu AS Status
@@ -45,18 +35,16 @@ SELECT DISTINCT *
 								AND oh.ohrel# = ol.olrel#)
 								Join custmast cm ON cm.cmcust = oh.ohcust
 							WHERE ohdtp = CURRENT_DATE
-								AND ohviac IN (''''1'''', ''''6'''')
-								AND ohticp = ''''Y''''
+								AND ohviac IN (''1'', ''6'')
+								AND ohticp = ''Y''
 								AND ohprlo = 50
-								AND ohotyp NOT IN (''''SA'''', ''''DP'''')
-								AND (olinvu = '''' '''' OR olinvu = ''''S'''' OR olinvu = ''''X'''' OR olinvu = ''''M'''') 
-								/*AND ohprtr IN (''''KSL1'''',''''KSL2'''')  */
-					'')
+								AND ohotyp NOT IN (''SA'', ''DP'')
+								AND (olinvu = '' '' OR olinvu = ''S'' OR olinvu = ''X'' OR olinvu = ''M'') 
+								/*AND ohprtr IN (''KSL1'',''KSL2'')  */
+					')
 ORDER BY [Status] DESC
 
 
-' 
-END
-GO
+
 
 
