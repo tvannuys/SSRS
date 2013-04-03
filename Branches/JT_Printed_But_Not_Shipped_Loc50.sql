a25c524e857c33d960230e05867cbd2e5ae7efa3
James Tuttle <jamest@tasupply.com>
2013-04-03 08:09:11 -0700

    SR# 9418 added customer name to a filed

--- Branches/JT_Printed_But_Not_Shipped_Loc50.sql
+++ Branches/JT_Printed_But_Not_Shipped_Loc50.sql
@@ -30,18 +30,20 @@ ALTER PROC [dbo].[JT_Printed_But_Not_Shipped_Loc50] AS
 /* Query to look at orders printed but not shipped for Will Call */
 
 SELECT DISTINCT *
-	FROM OPENQUERY (GSFL2K, ''SELECT ohprco AS CO,
-									ohprlo AS Loc,
-									ohord# AS Order,
-									ohrel# AS Release,
-									ohdtp AS Date_Printed,
-									ohviac AS Via_Code,
-									ohvia AS Via_Description,
-									olinvu AS Status
-							FROM oohead JOIN ooline ON ohco = olco
-								AND ohloc = olloc
-								AND ohord# = olord#
-								AND ohrel# = olrel#
+	FROM OPENQUERY (GSFL2K, ''SELECT cmname AS CustName
+									,ohprco AS CO
+									,ohprlo AS Loc
+									,ohord# AS Order
+									,ohrel# AS Release
+									,ohdtp AS Date_Printed
+									,ohviac AS Via_Code
+									,ohvia AS Via_Description
+								,olinvu AS Status
+							FROM oohead oh JOIN ooline ol ON (oh.ohco = ol.olco
+								AND oh.ohloc = ol.olloc
+								AND oh.ohord# = ol.olord#
+								AND oh.ohrel# = ol.olrel#)
+								Join custmast cm ON cm.cmcust = oh.ohcust
 							WHERE ohdtp = CURRENT_DATE
 								AND ohviac IN (''''1'''', ''''6'''')
 								AND ohticp = ''''Y''''
