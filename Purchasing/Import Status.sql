bc7238585e24b9e57c77155d0f7afa7dea255154
James Tuttle <jamest@tasupply.com>
2013-04-23 09:49:48 -0700

    Changed to a PROC

--- Purchasing/Import Status.sql
+++ Purchasing/Import Status.sql
@@ -1,79 +1,84 @@
 -- SR# 4765
 -- James Tuttle		Date:10/16/2012
 -- Add five vendors to the list
+-- SR# 9807 James Tuttle 04/15/2013 Added IMCLAS = 'IM' 
 
-select Buyer,OQ.VendorName,ProductCode,SKU,[Description],Color,Company,Location,PO,VendorRefNum,IssueDate,
-case ProductionDate
-	when '0001-01-01' then ''
-	else ProductionDate
-end as ProductionDate,
+CREATE PROC spImportStatus AS 
+BEGIN 
+	select Buyer,OQ.VendorName,ProductCode,SKU,[Description],Color,Company,Location,PO,VendorRefNum,IssueDate,
+	case ProductionDate
+		when '0001-01-01' then ''
+		else ProductionDate
+	end as ProductionDate,
 
-case ShipDate
-	when '0001-01-01' then ''
-	else ShipDate
-end as ShipDate,
+	case ShipDate
+		when '0001-01-01' then ''
+		else ShipDate
+	end as ShipDate,
 
-Confirmed,
+	Confirmed,
 
-case DueDate
-	when '0001-01-01' then ''
-	else DueDate
-end as DueDate,
+	case DueDate
+		when '0001-01-01' then ''
+		else DueDate
+	end as DueDate,
 
-case when Manifest IS null
-	then ''
-	else Manifest
-end as Manifest
+	case when Manifest IS null
+		then ''
+		else Manifest
+	end as Manifest
 
-from openquery(GSFL2K,'
+	from openquery(GSFL2K,'
 
-Select Poline.PLDDAT As DueDate, 
-PHDOI as IssueDate,
-PLSHIPDATE as ShipDate,
-PLRELDATE as ReleaseDate,
-PLPDAT as ProductionDate,
-PLBUYR as Buyer,
-PLDDATCONF as Confirmed,
-PHREF# as VendorRefNum,
-Poline.PLCO as Company,
-Poline.PLLOC As Location, 
-Family.FMFMCD as FamilyCode,
-Family.FMDESC as Family, 
-Itemmast.imprcd as ProductCode,
-ProdCode.pcdesc as ProductCodeDesc,
-Vendmast.VMNAME As VendorName, 
-Poline.PLPO# As PO, 
-Poline.PLITEM As SKU, 
-Poline.PLDESC As Description, 
-Itemmast.IMCOLR As Color, 
+	Select Poline.PLDDAT As DueDate, 
+	PHDOI as IssueDate,
+	PLSHIPDATE as ShipDate,
+	PLRELDATE as ReleaseDate,
+	PLPDAT as ProductionDate,
+	PLBUYR as Buyer,
+	PLDDATCONF as Confirmed,
+	PHREF# as VendorRefNum,
+	Poline.PLCO as Company,
+	Poline.PLLOC As Location, 
+	Family.FMFMCD as FamilyCode,
+	Family.FMDESC as Family, 
+	Itemmast.imprcd as ProductCode,
+	ProdCode.pcdesc as ProductCodeDesc,
+	Vendmast.VMNAME As VendorName, 
+	Poline.PLPO# As PO, 
+	Poline.PLITEM As SKU, 
+	Poline.PLDESC As Description, 
+	Itemmast.IMCOLR As Color, 
 
-(select max(mnman#) from manifest where mnpo# = poline.plpo#
-						and mnpolo = poline.plloc
-						and mnitem = poline.plitem
-						and mnpoco = poline.plco) as Manifest
+	(select max(mnman#) from manifest where mnpo# = poline.plpo#
+							and mnpolo = poline.plloc
+							and mnitem = poline.plitem
+							and mnpoco = poline.plco) as Manifest
 
-FROM Poline
+	FROM Poline
 
-left join Itemmast on Itemmast.IMITEM = Poline.PLITEM
-Left Join Pohead On (Poline.PLco = Pohead.PHco
-	and Poline.PLloc = Pohead.PHloc
-	and poline.plvend = pohead.phvend
-	and Poline.PLPO# = Pohead.PHPO#) 
+	left join Itemmast on Itemmast.IMITEM = Poline.PLITEM
+	Left Join Pohead On (Poline.PLco = Pohead.PHco
+		and Poline.PLloc = Pohead.PHloc
+		and poline.plvend = pohead.phvend
+		and Poline.PLPO# = Pohead.PHPO#) 
 
-Left Join Vendmast On Poline.PLVEND = Vendmast.VMVEND 
-Left Join Family On Poline.PLFMCD = Family.FMFMCD 
-Left Join ProdCode On Itemmast.imprcd = ProdCode.pcprcd
+	Left Join Vendmast On Poline.PLVEND = Vendmast.VMVEND 
+	Left Join Family On Poline.PLFMCD = Family.FMFMCD 
+	Left Join ProdCode On Itemmast.imprcd = ProdCode.pcprcd
 
-Where Pohead.PHDOI > ''12/31/2005''
-and FMFMCD not in (''L2'',''YI'')
-and IMSI = ''Y''
-and (poline.plvend in (''22666'',''22887'',''22674'',''22204'',''22859'',''23306'',''22312'',''16006'',''22179'',''24077'')
-	or (poline.plvend in(''21861'',''17000'',''10131'',''16006'') and imprcd in (''34057'',''4906'',''4906'',''6392'',''32608'')))
+	Where Pohead.PHDOI > ''12/31/2005''
+	/* and FMFMCD not in (''L2'',''YI'') */
+	and IMSI = ''Y''
+	AND IMCLAS = ''IM'' 
+	/*and (poline.plvend in (''22666'',''22887'',''22674'',''22204'',''22859'',''23306'',''22312'',''16006'',''22179'',''24077'')
+		or (poline.plvend in(''21861'',''17000'',''10131'',''16006'') and imprcd in (''34057'',''4906'',''4906'',''6392'',''32608''))) */
 
-Order By Poline.PLDDAT, Vendmast.VMNAME, Poline.PLPO#, Poline.PLITEM 
-') OQ
+	Order By Poline.PLDDAT, Vendmast.VMNAME, Poline.PLPO#, Poline.PLITEM 
+	') OQ
 
-Group by Buyer,OQ.VendorName,ProductCode,SKU,[Description],Color,Company,Location,
-PO,VendorRefNum,IssueDate,ProductionDate,ShipDate,Confirmed,DueDate,manifest
+	Group by Buyer,OQ.VendorName,ProductCode,SKU,[Description],Color,Company,Location,
+	PO,VendorRefNum,IssueDate,ProductionDate,ShipDate,Confirmed,DueDate,manifest
 
-Order by Buyer,Company,VendorName,ProductCode,SKU,IssueDate
+	Order by Buyer,Company,VendorName,ProductCode,SKU,IssueDate
+END
