e6ac0057db21a724d514f59593c4cba2ae2a7b0a
James Tuttle <jamest@tasupply.com>
2013-05-01 10:11:21 -0700

    SR# 10434

--- Inventory/JT_small_lot_vct_pm.sql
+++ Inventory/JT_small_lot_vct_pm.sql
@@ -1,31 +1,68 @@
 
---CREATE PROC JT_small_lot_vct_pm AS
+
 
 /* -----------------------------------------------------*
 ** James Tuttle 6/24/2011		Created: 6/25/2009		*
 ** -----------------------------------------------------*
-** 	Report is for Mannington VCT will QTY < 6			*
+** 	Report is for Armstrong VCT will QTY < 6			*
 **------------------------------------------------------*
 */
-
-
--- Query
-SELECT idco 'co',
-	idloc 'Loc',
-	iditem 'Item',
-	idserl 'Serial',
-	iddylt 'Dye Lot',
-	idbin 'Bin',
-	idqoh 'QTY'
+-- SR# 10434
+-- Need to GROUP BY Item and DyeLot then see if SUM is < 6 
+--
+--
+ALTER PROC JT_small_lot_vct_pm AS
+		SELECT *
+		FROM OPENQUERY(GSFL2K,'
+		  SELECT ibloc
+				,ibitem
+				,ibqoh
+				,ibqoo
+				,ibcls#
+		FROM itembal ib
+		WHERE ib.ibcls# = 41040
+			AND ibqoh > 0
+			AND ibqoo = 0
+			AND ibqoh BETWEEN 1 AND 5
+		ORDER BY ibloc
+				,ibitem
+		')
+/* ------
+  SELECT idco 
+	,idloc 
+	,iditem 
+	,idserl 
+	,iddylt 
+	,idbin 
+	,SUM(idqoh) AS Qoh
 
 FROM OPENQUERY (GSFL2K, '
-SELECT *
-FROM itemdetl
-WHERE idcls# = 41040
-	AND idqoh BETWEEN 1 AND 5
-	AND idqoo = 0
-')
+	SELECT idco
+		  ,idloc
+		  ,iditem
+		  ,idserl
+		  ,iddylt
+		  ,idbin
+		  ,idqoh
 
-ORDER BY idbin
+	FROM itemdetl
 
--- END Query
+	WHERE idcls# = 41040
+		AND idqoo = 0 
+		AND idqoh > 0
+				 		
+	')
+
+GROUP BY idco 
+		,idloc 
+		,iditem 
+		,idserl 
+		,iddylt 
+		,idbin 
+		
+HAVING SUM(idqoh) BETWEEN 1 AND 5	
+	
+ORDER BY idloc
+		,iditem
+		,idbin
+		--------------------------- */
