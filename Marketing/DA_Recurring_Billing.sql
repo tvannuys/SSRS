703f039ca04a8d44b76e8522df2b0bd261163073
James Tuttle <jamest@tasupply.com>
2013-05-08 13:32:34 -0700

    -- SR# 10660  James Tuttle  Date:05/08/2013 -- -- Change the name to Recurring Billing Report -- Change set-up to have the option of picking 1 or multiple item numbers -- Remove Cost column	- -- Remove Seq # column	- -- Remove Loc column	-

--- Marketing/DA_Recurring_Billing.sql
+++ Marketing/DA_Recurring_Billing.sql
@@ -10,25 +10,50 @@
 **																				**
 **																				**	
 **********************************************************************************/
+--
+-- SR# 10660  James Tuttle  Date:05/08/2013
+--
+-- Change the name to Recurring Billing Report
+-- Change set-up to have the option of picking 1 or multiple item numbers
+-- Remove Cost column	-
+-- Remove Seq # column	-
+-- Remove Loc column	-
+--
+---------------------------------------------------------------------------------------
 
-ALTER PROC DA_Recurring_Billing AS
+ALTER PROC DA_Recurring_Billing
+	 @CSV varchar(100)
+AS
 BEGIN
- SELECT *
+ SELECT RTRIM(REVERSE(SUBSTRING					
+			(REVERSE(cmadr3),3,LEN(cmadr3))))	AS City
+		,REVERSE(LEFT(REVERSE(cmadr3),2))		AS [State]
+		,rlcust									AS CustNbr
+		,cmname									AS Cust
+		,rlitem									AS Item	
+		,rldesc									AS [Description]
+		,rlqty									AS Qty
+		,rlpric									AS Price
+		
  FROM OPENQUERY(GSFL2K,	
 	'SELECT cmadr3
 		,rlcust
 		,cmname
-		,rlseq#
 		,rlitem
 		,rldesc
-		,rliloc
 		,rlqty
 		,rlpric
-		,rlcost
+
 	FROM custmast cm 
+	
 	RIGHT JOIN rbline rb ON rb.rlcust = cm.cmcust
 	')
+WHERE rlitem IN (SELECT * FROM dbo.udfCSVToLIst(@CSV))
 END
+
+-- DA_Recurring_Billing 'MAASPCER,MAASP,MAASPCOM'
+
+
 
 /*-------------------------------------------------------------------
 -- MS Access Query :: DA_Recurring Billing query
