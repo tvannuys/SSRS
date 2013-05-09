d5230159816b91548fcc7fd07f286536b3896209
James Tuttle <jamest@tasupply.com>
2013-05-09 07:47:30 -0700

    SR# 10634

---
+++ Inventory/JT_AdjustmentReport.sql
@@ -0,0 +1,43 @@
+/*********************************************************************************
+**																				**
+** SR# 10634																	**
+** Programmer: James Tuttle	Date:05/08/2013										**
+** ---------------------------------------------------------------------------- **
+** Purpose:		A report for Colleen that accepts these parameters:				**
+**					- Company													**
+**					- Location													**
+**					- Date range												**
+**					- Adjustment code											**
+**																				**
+**********************************************************************************/
+
+ALTER PROC JT_AdjustmentReport 
+	@co varchar(3)					-- Company
+	,@loc varchar(2)				-- Location
+	,@startDate varchar(10)			-- Start Date
+	,@endDate varchar(10)			-- End Date
+	,@adjustCode varchar(2)			-- Adjustment Code
+
+AS
+BEGIN
+DECLARE @sql varchar(4000) = '
+ SELECT *
+ FROM OPENQUERY(GSFL2K,	
+	''SELECT irco
+		,irloc
+		,MONTH(irdate) || ''''/'''' || DAY(irdate) || ''''/'''' || YEAR(irdate) AS aDate
+		,irreason
+		,irqty
+		,ircost
+
+	FROM itemrech ir
+	
+	WHERE ir.irco =  ' + '''' + '''' + @co + '''' + '''' + '
+		AND ir.irloc = ' + '''' + '''' + @loc + '''' + '''' + '
+		AND ir.irdate BETWEEN ' + '''' + '''' + @startDate + '''' + '''' + ' AND ' + '''' + '''' + @endDate + '''' + '''' + '
+		AND ir.irreason = ' + '''' + '''' + @adjustCode + '''' + '''' + '
+	'')'
+END
+EXEC(@sql)
+
+-- JT_AdjustmentReport 1,50,'05/01/2013','05/08/2013','02'
