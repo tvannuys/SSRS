a28149512f3216810aca8819d18b500468a0b4bc
James Tuttle <jamest@tasupply.com>
2013-06-11 12:41:59 -0700

    SR# 11596

--- Inside Sales - Customer Service/uspVerificationLineReport.sql
+++ Inside Sales - Customer Service/uspVerificationLineReport.sql
@@ -18,37 +18,30 @@ ALTER PROC uspVerificationLineReport AS
 BEGIN
  SELECT *
  FROM OPENQUERY(GSFL2K,	
-	'SELECT ohco
-		,ohord#
-		,ohrel#
-		,ohuser
-		,MONTH(ohodat) || ''/'' || DAY(ohodat)|| ''/'' || YEAR(ohodat) AS orderDate
-		,ohemds
-		,pbpo#
-		,MONTH(phdoi) || ''/'' || DAY(phdoi) || ''/'' || YEAR(phdoi) AS IssueDate
-		,MONTH(plpdat)|| ''/'' || DAY(plpdat)|| ''/'' || YEAR(plpdat) AS ProDate
-		,MONTH(plpdat)|| ''/'' || DAY(plpdat)|| ''/'' || YEAR(plpdat) AS dueDate
-		
+	'SELECT oh.ohco				As Comp
+		  ,oh.ohord#						AS OrderNbr
+		  ,oh.ohrel#						AS RelNbr
+		  ,oh.ohuser						AS CreatedBy
+		  ,ohemds							AS SubTotal
+		  ,MONTH(ohodat) || 
+			 ''/'' || DAY(ohodat)|| 
+			 ''/'' || YEAR(ohodat)			AS orderDate
+
 	FROM oohead oh
 	LEFT JOIN ooline ol ON (oh.ohco = ol.olco
 						AND oh.ohloc = ol.olloc
 						AND oh.ohord# = ol.olord#
 						AND oh.ohrel# = ol.olrel#
 						AND oh.ohcust = ol.olcust)
-	LEFT JOIN poboline po ON (po.pboco = ol.olco
-						AND po.pboloc = ol.olloc
-						AND po.pboord = ol.olord#
-						AND po.pborel = ol.olrel#
-						AND po.pbitem = ol.olitem)
-	LEFT JOIN poline pl ON (pl.plco = po.pbco
-						AND pl.plloc = po.pbloc
-						AND pl.plpo# = po.pbpo#
-						AND pl.plseq# = po.pbseq#
-						AND pl.plitem = po.pbitem)
-	LEFT JOIN pohead ph ON (ph.phco = pl.plco
-						AND ph.phloc = pl.plloc
-						AND ph.phpo# = pl.plpo#)
 						
 	WHERE ol.olitem IN (''VERIFICATION'',''VERIFICATIONPM'',''VERIFICATIONSAF'')
+	
+	ORDER BY oh.ohco
+			,oh.ohuser
+			,oh.ohord#	
 	')
 END
+
+
+-- uspVerificationLineReport
+	
