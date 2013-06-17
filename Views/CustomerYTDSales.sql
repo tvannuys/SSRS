d0569f04dece213466cecf93c9e94ec23a16dde1
tvannuys <thomasv@tasupply.com>
2013-06-17 15:59:38 -0700

    Revised to address questions related to MTD and YTD totals.  SR 11769

--- Views/CustomerYTDSales.sql
+++ Views/CustomerYTDSales.sql
@@ -12,32 +12,85 @@ GO
 
 alter VIEW [dbo].[CustomerYTDSales]
 AS
-SELECT dbo.CustomerSalesDetail.InvoiceDate, dbo.CustomerSalesDetail.Company, 
-               dbo.CustomerSalesDetail.SalesName, dbo.CustomerSalesDetail.BillToCustID, dbo.CustomerSalesDetail.BillToCustName, dbo.CustomerSalesDetail.BillToCity, 
-               dbo.CustomerSalesDetail.BillToState, dbo.CustomerSalesDetail.VendorNum, dbo.CustomerSalesDetail.VendorName, dbo.CustomerSalesDetail.FamilyCode, 
-               dbo.CustomerSalesDetail.FamilyCodeDesc, dbo.CustomerSalesDetail.Division, dbo.CustomerSalesDetail.DivisionDesc, dbo.SalesDivision.SalesDivision, 
-               SUM(dbo.CustomerSalesDetail.ExtendedPrice) AS Price, SUM(dbo.CustomerSalesDetail.ExtendedCost) AS Cost, SUM(dbo.CustomerSalesDetail.Profit) 
-               AS Profit
-FROM  dbo.CustomerSalesDetail LEFT OUTER JOIN
-               dbo.SalesDivision ON dbo.CustomerSalesDetail.FamilyCode = dbo.SalesDivision.FamilyCode
 
-WHERE 
-(
-	(InvoiceDate between DATEADD(yy, DATEDIFF(yy,0,GETDATE())-1, 0)
-	 and DATEADD(yy, -1, getdate()) 
+
+select Company,
+SalesName,
+BillToCustID,
+BillToCustName,
+BillToCity,
+BillToState,
+CONVERT(datetime, CONVERT(VARCHAR(10), sldate)) as InvoiceDate,
+VendorNum,
+VendorName,
+FamilyCode,
+FamilyCodeDesc,
+Division,
+DivisionDesc,
+Price,
+Cost,
+Profit
+
+from openquery(gsfl2k,'
+select  billto.cmcust as BillToCustID,
+billto.cmname as BillToCustName,
+Left(billto.CMADR3,23) as BillToCity,
+Right(billto.CMADR3,2) AS BillToState,
+shco as Company,
+smname as SalesName,
+sldate,
+SHLINE.SLVEND AS VendorNum,
+vmname as VendorName,
+SHLINE.SLFMCD AS FamilyCode, 
+FAMILY.FMDESC AS FamilyCodeDesc, 
+SHLINE.SLDIV AS Division, 
+DIVISION.DVDESC AS DivisionDesc, 
+sum(SLEPRC) AS Price, 
+sum(SLECST+SLESC1+SLESC2+SLESC3+SLESC4+SLESC5) as Cost,
+sum(sleprc-(SLECST+SLESC1+SLESC2+SLESC3+SLESC4+SLESC5)) AS Profit
+
+from shline
+		
+		left JOIN SHHEAD ON (SHLINE.SLCO = SHHEAD.SHCO 
+									AND SHLINE.SLLOC = SHHEAD.SHLOC 
+									AND SHLINE.SLORD# = SHHEAD.SHORD# 
+									AND SHLINE.SLREL# = SHHEAD.SHREL# 
+									AND SHLINE.SLINV# = SHHEAD.SHINV#) 
+		left JOIN CUSTMAST billto ON SHHEAD.SHBIL# = billto.CMCUST 
+		left join custmast soldto on shhead.shcust = soldto.cmcust
+		LEFT JOIN ITEMMAST ON SHLINE.SLITEM = ITEMMAST.IMITEM 
+		left join vendmast on slvend = vmvend
+		LEFT JOIN PRODCODE ON SHLINE.SLPRCD = PRODCODE.PCPRCD 
+		LEFT JOIN FAMILY ON SHLINE.SLFMCD = FAMILY.FMFMCD 
+		LEFT JOIN CLASCODE ON SHLINE.SLCLS# = CLASCODE.CCCLAS 
+		LEFT JOIN DIVISION ON SHLINE.SLDIV = DIVISION.DVDIV 
+		left join salesman on shline.SLSLMN = salesman.smno
+		
+where shhead.shco = 1
+and (
+	year(sldate)=year(current_date) or 
+	(year(sldate)=year(current_date)-1 and month(sldate) < month(current_date)) or
+ 	(year(sldate)=year(current_date)-1 and month(sldate) = month(current_date) and day(sldate) <= day(current_date))  
 	)
-or	(InvoiceDate between DATEADD(yy, DATEDIFF(yy,0,GETDATE()), 0) 
-	 and (getdate())
-	)
-)
-	
-AND dbo.CustomerSalesDetail.Company = 1
+and sldate >= current_date - 18 months
+
+group by billto.cmcust,
+billto.cmname,
+Left(billto.CMADR3,23),
+Right(billto.CMADR3,2),
+shco,
+smname,
+sldate,
+SHLINE.SLVEND,
+vmname,
+SHLINE.SLFMCD,
+FAMILY.FMDESC,
+SHLINE.SLDIV,
+DIVISION.DVDESC
 
 
-GROUP BY dbo.CustomerSalesDetail.InvoiceDate, dbo.CustomerSalesDetail.Company, 
-               dbo.CustomerSalesDetail.BillToCustID, dbo.CustomerSalesDetail.BillToCustName, dbo.CustomerSalesDetail.BillToCity, dbo.CustomerSalesDetail.BillToState, 
-               dbo.CustomerSalesDetail.VendorNum, dbo.CustomerSalesDetail.VendorName, dbo.CustomerSalesDetail.FamilyCode, dbo.CustomerSalesDetail.FamilyCodeDesc, 
-               dbo.CustomerSalesDetail.Division, dbo.CustomerSalesDetail.DivisionDesc, dbo.CustomerSalesDetail.SalesName, dbo.SalesDivision.SalesDivision
+')
+
 
 
 
