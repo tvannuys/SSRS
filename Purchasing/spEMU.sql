dfdb835f052d1641415fece11d7876a65271b249
tvannuys <thomasv@tasupply.com>
2013-06-26 09:42:20 -0700

    Initial load

---
+++ Purchasing/spEMU.sql
@@ -0,0 +1,152 @@
+--spEMU 'GRTB1211BR'
+
+alter proc spEMU 
+
+@Item as varchar(30)
+
+as
+
+/* =================================================
+
+	First date range - gather total sales 
+
+====================================================*/
+
+declare @sql as varchar(5000)
+
+set @sql = 'select * into #EMU1
+
+from openquery(gsfl2k,''
+
+select  slco,
+slloc,
+slitem,
+year(SHiDAT),
+month(SHiDAT),
+/* sum(SLBLUS) as QtySold */
+sum(SLQSHP) as QtySold 
+
+from shline 
+join shhead on (shhead.shco = shline.slco
+	and shhead.shloc = shline.slloc
+	and shhead.shord# = shline.slord#
+	and shhead.shrel# = shline.slrel#
+	and shhead.shinv# = shline.slinv#
+	and shhead.shcust = shline.slcust)
+join itemmast on shline.slitem = itemmast.imitem
+join itemxtra on itemmast.imitem = itemxtra.imxitm
+
+where imitem = ' + '''''' + @Item + '''''' + 
+' and shidat >= year(current_date - 4 months) || ''''-'''' || month(current_date - 4 months) || ''''-1''''
+and shidat < year(current_date - 1 months) || ''''-'''' || month(current_date - 1 months) || ''''-1''''
+
+
+group by slco,
+slloc,
+slitem,
+year(SHiDAT),
+month(SHiDAT)
+
+
+'')
+
+/* =================================================
+
+	Second date range - gather total sales 
+
+====================================================*/
+
+select * into #EMU2 from openquery(gsfl2k,''
+
+select  slco,
+slloc,
+slitem,
+year(SHiDAT),
+month(SHiDAT),
+/* sum(SLBLUS) as QtySold */
+sum(SLQSHP) as QtySold 
+
+
+from shline 
+join shhead on (shhead.shco = shline.slco
+	and shhead.shloc = shline.slloc
+	and shhead.shord# = shline.slord#
+	and shhead.shrel# = shline.slrel#
+	and shhead.shinv# = shline.slinv#
+	and shhead.shcust = shline.slcust)
+join itemmast on shline.slitem = itemmast.imitem
+join itemxtra on itemmast.imitem = itemxtra.imxitm
+
+where imitem = ' + '''''' + @Item + '''''' + 
+' and shidat >= year(current_date - 3 months) || ''''-'''' || month(current_date - 3 months) || ''''-1''''
+and shidat < year(current_date - 0 months) || ''''-'''' || month(current_date - 0 months) || ''''-1''''
+
+
+group by slco,
+slloc,
+slitem,
+year(SHiDAT),
+month(SHiDAT)
+
+'')
+
+/* =================================================
+
+	Calc average 1, put in temp table
+
+====================================================*/
+
+select slco,
+slloc,
+slitem,
+sum(qtysold)/3 as AvgSold
+
+into #emuAvg1
+
+from #emu1
+
+group by slco,
+slloc,
+slitem
+
+/* =================================================
+
+	Calc average 2, put in temp table
+
+====================================================*/
+
+
+select slco,
+slloc,
+slitem,
+sum(qtysold)/3 as AvgSold
+
+into #emuAvg2
+
+from #emu2
+
+group by slco,
+slloc,
+slitem
+
+/* =================================================
+
+   Final Select
+
+====================================================*/
+
+
+select (sum(a.AvgSold) + sum(B.AvgSold)) /2 as EMU
+
+from #emuAvg1 a
+join #emuAvg2 b on (a.slco = b.slco
+	and a.slloc = b.slloc
+	and a.slitem = b.slitem)
+	
+where ((a.AvgSold + B.AvgSold) /2) / 4 <> 0
+
+
+'
+
+--select @sql
+exec(@sql)
