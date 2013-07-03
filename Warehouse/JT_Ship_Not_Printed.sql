aecf2b767c55b2ebe70ed1aeaf58469ebe1034d0
James Tuttle <jamest@tasupply.com>
2013-07-03 13:39:12 -0700

    Initial file load

---
+++ Warehouse/JT_Ship_Not_Printed.sql
@@ -0,0 +1,31 @@
+/*********************************************************************************
+**										**
+** SR# nnnn									**
+** Programmer: James Tuttle						**
+** ---------------------------------------------------------------------------- **
+** Purpose:									**
+**										**
+**										**
+**										**
+**										**
+**										**
+**********************************************************************************/
+
+CREATE PROC JT_Ship_Not_Printed AS
+BEGIN
+ SELECT *
+ FROM OPENQUERY(GSFL2K,	
+	'SELECT *
+
+	FROM oohead oh
+	Left Join ooline ol ON (oh.ohco = ol.olco
+						AND oh.ohloc = ol.olloc
+						AND oh.ohord# = ol.olord#
+						AND oh.ohrel# = ol.olrel#)
+						
+	WHERE (OHTICP = ''N'' OR OHTICP = '' '')
+		AND olINVU = ''T''
+		AND ohotyp NOT IN (''IR'',''SI'',''SA'',''DP'')
+		AND oldirs != ''Y''
+	')
+END
