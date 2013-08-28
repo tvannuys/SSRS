ed1dc858c5c1c6d86f21239a6401587c08f6350d
tvannuys <thomasv@tasupply.com>
2013-08-28 14:33:09 -0700

    SR 13792

---
+++ Finance/GL AC Transactions.sql
@@ -0,0 +1,12 @@
+select * from openquery(gsfl2k,'
+select m.gldesc, g.gdco,g.gdgl#, month(gddate) as "GL Month", year(gddate) as "GL Year",sum(gdamt) as Amount
+
+from gldetl g
+left join GLMASTGL m on m.glco = g.gdco    and m.glgl# = g.gdgl#
+
+where gddate > current_date - 18 months
+and gdjrfc = ''AC''
+and gdgl# >= 600000
+
+group by m.gldesc, g.gdco,g.gdgl#, month(gddate), year(gddate)
+')
