efd5262ed260e1cf097df22848025c242e2a2a52
tvannuys <thomasv@tasupply.com>
2013-09-10 10:44:21 -0700

    Initial check in

---
+++ Sales/OE Customer Review Base Data.sql
@@ -0,0 +1,44 @@
+select *,CONVERT(datetime, CONVERT(VARCHAR(10), shidat)) as InvoiceDate
+from openquery(gsfl2k,'
+select  shco,shloc,shord#,shrel#
+shinv#,
+shidat,
+sldate,
+soldto.cmcust as SoldToCustID,
+soldto.cmname as SoldToCust,
+billto.cmcust as BillToCustID,
+billto.cmname as BillToCust,
+imfmcd as FamilyCode,
+fmdesc as Family,
+dvdesc as Division,
+pcname as ProductCode,
+slitem,
+imdesc,
+SLECST+SLESC1+SLESC2+SLESC3+SLESC4+SLESC5 as ExtendedCost,
+sleprc as ExtendedPrice
+
+from shline
+		
+		left JOIN SHHEAD ON (SHLINE.SLCO = SHHEAD.SHCO 
+									AND SHLINE.SLLOC = SHHEAD.SHLOC 
+									AND SHLINE.SLORD# = SHHEAD.SHORD# 
+									AND SHLINE.SLREL# = SHHEAD.SHREL# 
+									AND SHLINE.SLINV# = SHHEAD.SHINV#) 
+		left JOIN CUSTMAST billto ON SHHEAD.SHBIL# = billto.CMCUST 
+		left join custmast soldto on shhead.shcust = soldto.cmcust
+		LEFT JOIN ITEMMAST ON SHLINE.SLITEM = ITEMMAST.IMITEM 
+		left join vendmast on slvend = vmvend
+		LEFT JOIN PRODCODE ON SHLINE.SLPRCD = PRODCODE.PCPRCD 
+		LEFT JOIN FAMILY ON SHLINE.SLFMCD = FAMILY.FMFMCD 
+		LEFT JOIN CLASCODE ON SHLINE.SLCLS# = CLASCODE.CCCLAS 
+		LEFT JOIN DIVISION ON SHLINE.SLDIV = DIVISION.DVDIV 
+		left join salesman on shline.SLSLMN = salesman.smno
+		
+where year(shidat) in (2012,2013)
+
+and billto.cmcust in (''1006826'',''1012140'')
+
+
+')
+
+
