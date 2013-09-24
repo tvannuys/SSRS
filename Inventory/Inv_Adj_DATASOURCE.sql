f0774de99d7b769b32371490da4c19d218f778e6
James Tuttle <jamest@tasupply.com>
2013-09-24 07:01:40 -0700

    Changes on CASE

--- Inventory/Inv_Adj_DATASOURCE.sql
+++ Inventory/Inv_Adj_DATASOURCE.sql
@@ -6,24 +6,31 @@
 
 ALTER PROC Inv_Adj_DATASOURCE AS
 
-SELECT irco
-		,irloc
-		,irreason
-		,iritem
-		,imdesc
-		,irky
-		,irDate
+-- commentied out fields. Was used for debugging and comparison
+
+SELECT irco			AS Co
+		,irloc		AS Loc
+		,irreason	AS ReasonCode
+		,iritem		AS Item
+		,imdesc		AS [Description]
+		,irky		AS IRTag
+		,irDate		AS [Date]
 		,[Month]
 		,[Year]
-		,irserl
-		,irdylt
-		,ircomt
-		,irqty
-		,IRBLU
-		,ircost															
-		,Total
+		,irserl		AS Serial
+		,irdylt		AS DyeLot
+		,ircomt		AS Comment
+		--,irqty	AS Qty
+		,Qty
+		,UM
+		,imum1
 		,irum1
-		,iruser
+		--,irblu	AS BillableUnits
+		--,irum2	AS BillableUM
+		,ircost		AS Cost														
+		,TotalCost
+		--,imum1
+		,iruser		AS [User]
 
 FROM OPENQUERY(GSFL2K,'
 	SELECT irco
@@ -39,16 +46,37 @@ FROM OPENQUERY(GSFL2K,'
 		,irdylt
 		,ircomt
 		
+/* ---- Show each or billable units	------------------------------------------------------- */
 		,CASE
-			WHEN (immd = ''M'' AND immd2 = '' '') 
-				THEN CAST(ROUND((irqty * imfact) * ircost,3) AS decimal (18,2))
-			WHEN (immd = ''M'' AND immd2 = ''D'') 
-				THEN CAST(ROUND(((irqty * imfact) / imfac2) * ircost,3) AS decimal (18,2))
-			ELSE 0
-		 END AS Total
+			WHEN irblu = 0
+				THEN irqty
+			ELSE irblu
+		END AS Qty
+		
+/* ---- Logic for using Unit Bill when adjustment done and the system converts outside on ITEMMAST else use ITEMMAST conversion ---- */		
+
+		,CASE
+			WHEN irblu = 0 THEN
+			    CASE 
+					WHEN (immd = ''M'' AND immd2 = '' '') 
+						THEN CAST(ROUND((irqty * imfact) * ircost,3) AS decimal (18,2))
+					WHEN (immd = ''M'' AND immd2 = ''D'') 
+						THEN CAST(ROUND(((irqty * imfact) / imfac2) * ircost,3) AS decimal (18,2))
+					END
+			ELSE CAST(ROUND(irblu * ircost,3) AS decimal (18,2))
+		 END AS TotalCost
 		 
+/* ---- If use billable units conversion then display the U/M by the conversion ELSE use the ITEMMAST conversion ---- */	
+	 
+		,CASE
+			WHEN irum1 IS NOT NULL 
+				THEN irum2
+			ELSE imum1
+		 END AS UM
+/* ------------------------------------------------------------------------------------------------------ */		 
+		,imum1	
 		,IRBLU
-		 
+		,irum2
 		,irqty
 		,ircost
 		,immd
@@ -62,8 +90,11 @@ FROM itemrech ir
 LEFT JOIN itemmast im ON im.imitem = ir.iritem
 
 WHERE irloc IN(41, 50, 52, 57)
-	AND irreason IN(''25'', ''39'', ''02'',''38'')
-	AND irdate = ''08/20/2013''
+	AND irreason IN(''25'', ''39'', ''02'',''38'',''PI'')
+	AND irdate >= (CURRENT_DATE + 1 DAYS - DAY(CURRENT_DATE + 1 MONTH)DAY) /* - 3 YEARS */
+	
+	/* AND irdate = ''08/20/2013'' */
+	/*  AND iritem IN (''ARX20702'',''JTMIRA24X24ROSS'') */
 
 GROUP BY irco
 		,irloc
@@ -86,6 +117,8 @@ GROUP BY irco
 		,imfact
 		,imfac2
 		,IRBLU
+		,irum2
+		,imum1
 	
 ORDER BY irco
 			,irloc
