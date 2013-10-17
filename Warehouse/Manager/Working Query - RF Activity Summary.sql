191328b0f8bd79e613c6a8c4e2106406b0988db0
James Tuttle <jamest@tasupply.com>
2013-10-17 14:55:54 -0700

    Turned into a CTE instead of using TEMP Tables

--- Warehouse/Manager/Working Query - RF Activity Summary.sql
+++ Warehouse/Manager/Working Query - RF Activity Summary.sql
@@ -1,112 +1,92 @@
-drop table #temptotalAct
-drop table #Temptotalhours  
+/*******************************************************************************
+**	James Tuttle
+**  10/17/2013
+** -----------------------------------------------------------------------------
+** Query as a Datasource for RF Daily Activity for management that will allow
+** to select Employee Name, Total Hours, Total Actvites, and then have an average
+** Calculated field in Excel Pivot Table.
+********************************************************************************/
+--------------------------------------------------------------------------------
+--  CTE # 1 for all total activities [EXCEPT Total Hours]
+--------------------------------------------------------------------------------
+WITH CTE_TotalActivities AS (
+	SELECT p.company
+		  ,p.location
+		  ,p.rfdate
+		  ,DATENAME(DW,p.rfdate) as [Day]
+		  ,DATEPART(M,p.rfdate) as [Month]
+		  ,DATEPART(WK,p.rfdate) as [Week]
+		  ,DATEPART(YY,p.rfdate) as [Year]
+		  ,u.EMPLOYEENAME
+		  ,SUM(CONVERT(float, p.Value)) as TotalActivities
 
-SELECT p.company
-      ,p.location
---      ,p.Dept
---      ,p.RFUser
---      ,p.Metric
---      ,CONVERT(float, p.Value) as Value
-      ,p.rfdate
---      ,p.Daily
-      ,DATENAME(DW,p.rfdate) as [Day]
-      ,DATEPART(M,p.rfdate) as [Month]
-      ,DATEPART(WK,p.rfdate) as [Week]
-      ,DATEPART(YY,p.rfdate) as [Year]
-      ,u.EMPLOYEENAME
-      ,SUM(CONVERT(float, p.Value)) as TotalActivities
-
-into #TempTotalAct
-
-FROM WarehouseProductivity p WITH (INDEX(NCI_WarehouseProductivity_RFDate_Metric_Daily))
-LEFT JOIN WarehouseUsers u on u.OLRUSR = p.RFUser
-WHERE RFDate between '10/15/2011' and GETDATE()
-      AND (Metric like 'weight%')
---      OR Metric like 'hours%')
-      AND Metric not like 'WCC Avg Time%'
-      
-      
-group by p.company
-      ,p.location
---      ,p.Dept
---      ,p.RFUser
---      ,p.Metric
---      ,CONVERT(float, p.Value) 
-      ,p.rfdate
---      ,p.Daily
-      ,DATENAME(DW,p.rfdate) 
-      ,DATEPART(M,p.rfdate) 
-      ,DATEPART(WK,p.rfdate) 
-      ,DATEPART(YY,p.rfdate) 
-      ,u.EMPLOYEENAME
-
-      
-ORDER BY DATEPART(wK,p.rfdate)
-            ,RFDate
-            ,company
-            ,location
+	FROM WarehouseProductivity p WITH (INDEX
+					(NCI_WarehouseProductivity_RFDate_Metric_Daily))
+	LEFT JOIN WarehouseUsers u on u.OLRUSR = p.RFUser
+	WHERE RFDate BETWEEN '10/15/2011' AND GETDATE()
+		  AND (Metric LIKE 'weight%')
+		  AND Metric NOT LIKE 'WCC Avg Time%'
+	            
+	GROUP BY p.company
+		  ,p.location
+		  ,p.rfdate
+		  ,DATENAME(DW,p.rfdate) 
+		  ,DATEPART(M,p.rfdate) 
+		  ,DATEPART(WK,p.rfdate) 
+		  ,DATEPART(YY,p.rfdate) 
+		  ,u.EMPLOYEENAME
+ )
             
-/**********************************************************/
+--------------------------------------------------------------------------------
+--  CTE # 2 for Total Hours ONLY
+--------------------------------------------------------------------------------
 
-SELECT p.company
-      ,p.location
---      ,p.Dept
---      ,p.RFUser
---      ,p.Metric
---      ,CONVERT(float, p.Value) as Value
-      ,p.rfdate
---      ,p.Daily
-      ,DATENAME(DW,p.rfdate) as [Day]
-      ,DATEPART(M,p.rfdate) as [Month]
-      ,DATEPART(WK,p.rfdate) as [Week]
-      ,DATEPART(YY,p.rfdate) as [Year]
-      ,u.EMPLOYEENAME
-      ,SUM(CONVERT(float, p.Value)) as TotalHours
+ ,CTE_TotalHours AS (
+	SELECT p.company
+		  ,p.location
+		  ,p.rfdate
+		  ,DATENAME(DW,p.rfdate) as [Day]
+		  ,DATEPART(M,p.rfdate) as [Month]
+		  ,DATEPART(WK,p.rfdate) as [Week]
+		  ,DATEPART(YY,p.rfdate) as [Year]
+		  ,u.EMPLOYEENAME
+		  ,SUM(CONVERT(float, p.Value)) as TotalHours   
 
-into #TempTotalHours      
-
-FROM WarehouseProductivity p WITH (INDEX(NCI_WarehouseProductivity_RFDate_Metric_Daily))
-LEFT JOIN WarehouseUsers u on u.OLRUSR = p.RFUser
-WHERE RFDate between '10/15/2011' and GETDATE()
---      AND (Metric like 'weight%')
-      and Metric like 'hours%'
-      AND Metric not like 'WCC Avg Time%'
-      
-      
-group by p.company
-      ,p.location
---      ,p.Dept
-      
---      ,p.Metric
---      ,CONVERT(float, p.Value) 
-      ,p.rfdate
---      ,p.Daily
-      ,DATENAME(DW,p.rfdate) 
-      ,DATEPART(M,p.rfdate) 
-      ,DATEPART(WK,p.rfdate) 
-      ,DATEPART(YY,p.rfdate) 
-      ,u.EMPLOYEENAME
-
-      
-ORDER BY DATEPART(wK,p.rfdate)
-            ,RFDate
-            ,company
-            ,location
+	FROM WarehouseProductivity p WITH (INDEX
+					(NCI_WarehouseProductivity_RFDate_Metric_Daily))
+	LEFT JOIN WarehouseUsers u on u.OLRUSR = p.RFUser
+	WHERE RFDate BETWEEN '10/15/2011' AND GETDATE()
+		  AND Metric LIKE 'hours%'
+		  AND Metric NOT LIKE 'WCC Avg Time%'
+	          
+	GROUP BY p.company
+		  ,p.location
+		  ,p.rfdate
+		  ,DATENAME(DW,p.rfdate) 
+		  ,DATEPART(M,p.rfdate) 
+		  ,DATEPART(WK,p.rfdate) 
+		  ,DATEPART(YY,p.rfdate) 
+		  ,u.EMPLOYEENAME
+ )
             
-            
-/***********************************************************/
+--------------------------------------------------------------------------------
+--  Join the two CTEs to get the results into one
+--------------------------------------------------------------------------------
 
-select t1.Company,t1.EMPLOYEENAME,t1.Month,t1.Year,t1.Day,t1.TotalHours,
-isnull(t2.TotalActivities,0) as Activities
+SELECT t1.Company
+	  ,t1.EMPLOYEENAME
+	  ,t1.Month,t1.Year
+	  ,t1.Day,t1.TotalHours
+	  ,ISNULL(t2.TotalActivities,0) AS Activities
 
-from #TempTotalHours T1
-left join #TempTotalAct T2 on (t1.Company = t2.Company and
-								t1.Day = t2.Day and
-								
-								t1.EMPLOYEENAME = t2.EMPLOYEENAME and
-								
-								t1.Month = t2.Month and
-								t1.RFDate = t2.RFDate and
-								t1.Week = t2.Week and
-								t1.Year = t2.Year)
-								
+FROM CTE_TotalHours T1
+LEFT JOIN CTE_TotalActivities T2 on (t1.Company = t2.Company 
+									AND t1.Day = t2.Day 
+									AND t1.EMPLOYEENAME = t2.EMPLOYEENAME
+									AND t1.Month = t2.Month
+									AND t1.RFDate = t2.RFDate
+									AND t1.Week = t2.Week
+									AND t1.Year = t2.Year)
+--------------------------------------------------------------------------------
+--    END    CTE Expressions 
+--------------------------------------------------------------------------------								
