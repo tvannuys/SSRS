dea5fa237c268a253962bc552ee6fbc4b71a6a84
James Tuttle <jamest@tasupply.com>
2013-11-07 14:28:36 -0800

    Added the OOINUSE FILE and Inovice pgms (OE100 and OE105)

--- Finance/JT_orders_without_inventory.sql
+++ Finance/JT_orders_without_inventory.sql
@@ -16,18 +16,24 @@ FROM OPENQUERY (GSFL2K, 'SELECT ohotyp as order_type,
 								olbyp as bypass,
 								ohuser as user
 								
-						FROM oohead oh INNER JOIN ooline ol
+						FROM oohead oh 
+						LEFT JOIN ooline ol
 							ON (oh.ohco = ol.olco
 							AND oh.ohloc = ol.olloc
 							AND oh.ohord# = ol.olord#
 							AND oh.ohrel# = ol.olrel#)
-						JOIN itemmast im ON ol.olitem = im.imitem
+						LEFT JOIN itemmast im ON ol.olitem = im.imitem
+						LEFT JOIN ooinuse in ON (in.iuco = oh.ohco
+											AND in.iuloc = oh.ohloc
+											AND in.iuord# = oh.ohord#
+											AND in.iurel# = oh.ohrel#)
 						
 						WHERE ol.olbyp = ''N''
 							AND ol.olinvu = ''T''  
 							AND oh.ohotyp NOT IN(''CL'', ''FC'')
 							AND im.imsi != ''O''
 							AND oh.ohcred NOT IN (''MP'',''AP'')
+							AND iupgm IN (''OE100'', ''OE105'')
 							
 						ORDER BY ohco, ohloc
 						
