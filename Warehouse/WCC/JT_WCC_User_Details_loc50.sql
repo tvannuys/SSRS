d4998022cd639132eeb628d8c1a16f17b9120f55
James Tuttle <jamest@tasupply.com>
2013-11-15 07:19:10 -0800

    Cleanup

--- Warehouse/WCC/JT_WCC_User_Details_loc50.sql
+++ Warehouse/WCC/JT_WCC_User_Details_loc50.sql
@@ -2,28 +2,80 @@ USE [GartmanReport]
 GO
 
 
--- James Tuttle
--- 05/15/2012
--- WCC Details for the average Time
---
+/*********************************************************************************
+**																				**
+** SR# 14499																	**
+** Programmer: James Tuttle	Date:09/25/2013										**
+** ---------------------------------------------------------------------------- **
+** Purpose:																		**
+**																				**
+**																				**
+**																				**
+**																				**
+**																				**
+**********************************************************************************/
 
-ALTER PROC [dbo].[JT_WCC_User_Details_loc50] AS
+
+ALTER PROC [dbo].[JT_WCC_User_Details_loc50] 
+		    @FromDate varchar(10)
+		   ,@ToDate varchar(10)
+		   ,@CSV varchar(15)
+AS
+BEGIN
+DECLARE @sql varchar(3000) = '
+SELECT LTRIM(SUBSTRING(emname,CHARINDEX('' '', emname)+1, LEN(emname))) AS [First]
+	  ,SUBSTRING(emname,1, CHARINDEX('' '',emname)-1)					  AS [Last]
+	  ,LineCount
+	FROM OPENQUERY (GSFL2K,''SELECT	emname
+						   ,COUNT(rfoitem) AS LineCount	
+						   ,rfloc
+								
+							FROM rfwillchst wc
+							LEFT JOIN userxtra ux ON ux.usxid = wc.rfouser
+							LEFT JOIN prempm em on em.ememp# = ux.usxemp#
+							
+							WHERE  rfodate >= '+ '''' + '''' +  @FromDate + '''' + '''' + '
+								AND rfodate <= '+ '''' + '''' + @ToDate + '''' + '''' + '
+								AND rpstat = ''''T''''
+								AND rfobin# != ''''SHIPD''''
+							
+							GROUP BY emname
+								    ,rfloc
+							
+							ORDER BY COUNT(rfoitem)  DESC								
+							'')
+	/* @CSV to pass in a list of multiple PARMS from the SSRS */
+	WHERE rfloc IN(SELECT * FROM dbo.udfCSVToList(''' + @CSV + ''')) 
+	
+	'
+END
+EXEC (@sql)
+GO
+------->>>    JT_WCC_User_Details_loc50 '09/30/2013','09/30/2013','42'
+
+/*
+------------------------- DETAILS -----------------------------------------------
 
 SELECT *
-	FROM OPENQUERY (GSFL2K,'SELECT	rfouser
-									,COUNT(rfoitem) AS LineCount	
+	FROM OPENQUERY (GSFL2K,'SELECT	rfloc
+									 
+									,emname
+									,rfoord#
+									,rfoitem	
 								
-							FROM rfwillchst
+							FROM rfwillchst wc
+							LEFT JOIN userxtra ux ON ux.usxid = wc.rfouser
+							LEFT JOIN prempm em on em.ememp# = ux.usxemp#
 							
-							WHERE rfloc = 50
-								AND rfodate = CURRENT_DATE 
+							WHERE rfloc = 60
+								AND rfodate >= ''09/30/2013''
+								AND rfodate <= ''09/30/2013''
 								AND rpstat = ''T''
 								AND rfobin# != ''SHIPD''
 							
-							GROUP BY rfouser
+							
+							
+							ORDER BY emname
 								
 					')
-
-GO
-
-
+------------------------------------------------------------------------------------------ */
