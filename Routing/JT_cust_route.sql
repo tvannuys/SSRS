89eb47d9d1c5fdb264bdb020c5055fafcfd9342d
James Tuttle <jamest@tasupply.com>
2013-11-20 10:25:00 -0800

    Reverted back to the original copy for now

--- Routing/JT_cust_route.sql
+++ Routing/JT_cust_route.sql
@@ -45,7 +45,48 @@ SET @SQL ='
 	EXEC (@SQL)
 	
 	-- JT_cust_route ORCC2
+
+
+--ALTER PROC JT_cust_route 
+--	-- @Route AS varchar(5) = '%'
+--	@CSV varchar(100)			-- For multiple PARMS
+--AS
+
+--DECLARE @SQL AS varchar(3000)
+
+
+
+--SET @SQL ='
+--	SELECT cmname AS Cust_Name
+--			,cmcust As Customer#
+--			,cmvia AS Via_Code
+--			,cmdrt1 AS Route_1
+--			,cmdrt2 AS Route_2
+--			,RTRIM(REVERSE(SUBSTRING(REVERSE(CMADR3),3,LEN(CMADR3)))) AS City
+--			,REVERSE(LEFT(REVERSE(CMADR3),2)) AS State
+--			,cmzip AS Zip
+--	FROM OPENQUERY(GSFL2K,
+--		''SELECT cmname
+--				,cmcust
+--				,cmvia
+--				,cmdrt1
+--				,cmdrt2
+--				,cmadr3
+--				,cmzip
+--	FROM custmast cm
 	
+--	ORDER BY cm.cmname
+	
+--	'')
+--	WHERE (cmdrt1 IN (SELECT * FROM dbo.udfCSVToList(@CSV)))
+--		OR (cmdrt2 IN (SELECT * FROM dbo.udfCSVToList(@CSV)))
+--'
+	
+--	EXEC (@SQL)
+	
+	-- JT_cust_route 'ORCC2,OCC2P'
+	
+	/* WHERE cm.cmdrt1 = ' + '''' + '''' + @Route + '''' + '''' + '  */
 -------------------------
 --
 -- need to allow lookup
