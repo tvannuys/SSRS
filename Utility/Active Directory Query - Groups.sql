1464d9d18e16659565415ec8134d7dd19de76c01
tvannuys <thomasv@tasupply.com>
2013-11-29 10:52:09 -0800

    Queries to interigate the AD via SQL

---
+++ Utility/Active Directory Query - Groups.sql
@@ -0,0 +1,48 @@
+/* Query the Active Directory AD and get the members of a Group 
+
+Change the value of @GroupName as necessary
+
+*/
+
+declare @GroupName varchar(50)
+declare @sql varchar(max)
+
+set @GroupName = 'All T&A'
+set @GroupName = 'orderscancelled'
+
+set @sql = 'SELECT *
+FROM OPENQUERY(ADSI,''<LDAP://DC=tasupply,DC=com>;
+(&(objectCategory=user)(memberOf=CN=' + @GroupName + ',CN=Users,DC=tasupply,DC=com));
+cn,name;subtree'')'
+
+exec(@sql)
+
+
+
+
+
+/* Query the Active Directory AD and get the members of a Group 
+
+Version with no variables
+
+*/
+
+SELECT
+*
+FROM OPENQUERY(ADSI,'<LDAP://DC=tasupply,DC=com>;
+(&(objectCategory=user)(memberOf=CN=Management-s,CN=Users,DC=tasupply,DC=com));
+cn,name;subtree')
+
+
+/* Get a list of Groups */
+
+
+select  *
+from  openquery(ADSI, '
+select  samaccountname,mail,sn,name, cn, objectCategory,distinguishedName 
+from    ''LDAP://dc=tasupply,dc=com''
+where   objectCategory=''group''
+      
+')
+
+
---
+++ Utility/Active Directory Query.sql
@@ -0,0 +1,39 @@
+select  mail,
+displayname,
+samAccountName,
+distinguishedname,
+pwdLastSet,
+userAccountControl as PasswordNeverExpires
+
+
+from  openquery(ADSI, '
+select  givenName,
+				sn,
+				sAMAccountName,
+				displayName,
+				mail,
+				telephoneNumber,
+				mobile,
+				physicalDeliveryOfficeName,
+				department,
+				division,
+				pwdLastSet,
+				userAccountControl,
+				profilePath,
+				distinguishedname
+
+
+
+from    ''LDAP://dc=tasupply,dc=com''
+where   objectCategory = ''Person''
+        and
+        objectClass = ''user''
+')
+
+where displayname like '%nora%'
+or displayname like '%mary%'
+or displayname like '%judi%'
+or displayname like '%thomas%'
+or displayname like '%owen%'
+
+
