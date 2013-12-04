9e68af86cf5f4731113b56a42bd4976dfca5e056
James Tuttle <jamest@tasupply.com>
2013-12-04 15:11:54 -0800

    SR# 16301

--- Inside Sales - Customer Service/uspVerificationLineReportManager.sql
+++ Inside Sales - Customer Service/uspVerificationLineReportManager.sql
@@ -14,13 +14,15 @@
 **																				**
 **********************************************************************************/
 
+-- SR#16301 James Tuttle 12/04/2013
+
 ALTER PROC uspVerificationLineReportManager AS
 BEGIN
  SELECT *
  FROM OPENQUERY(GSFL2K,	
 	' WITH OO AS (SELECT ohco					AS Co
 					,ohord#						AS Order
-					,ohrel#						AS Rel					
+	 /*				,ohrel#						AS Rel		*/			
 					
 					FROM oohead oh
 					LEFT JOIN ooline ol ON (oh.ohco = ol.olco
@@ -29,28 +31,29 @@ BEGIN
 						AND oh.ohrel# = ol.olrel#
 						AND oh.ohcust = ol.olcust)
 					
-					WHERE ol.olitem IN (''VERIFICATION'',''VERIFICATIONPM'',''VERIFICATIONSAF'') )
+					WHERE ol.olitem IN (''VERIFICATION'',''VERIFICATIONPM'',''VERIFICATIONSAF'',''VERIFICATION80'')
+				  )
 
 	SELECT DISTINCT oh2.ohco				As Comp
 		  ,oh2.ohord#						AS OrderNbr
-		  ,oh2.ohrel#						AS RelNbr
+	/*	  ,oh2.ohrel#						AS RelNbr	*/
 		  ,oh2.ohuser						AS CreatedBy
-		  ,ohemds							AS SubTotal
+		  ,SUM(oleprc)						AS SubTotal
 		  ,MONTH(ohodat) || 
 			 ''/'' || DAY(ohodat)|| 
 			 ''/'' || YEAR(ohodat)			AS orderDate
-		  ,pbco								AS PoCo
-		  ,pbloc							AS POLoc
-		  ,pbpo#							AS PO#
-		  ,MONTH(phdoi) || ''/'' 
-			|| DAY(phdoi) || ''/'' || 
-			YEAR(phdoi)						AS IssueDate
-		  ,MONTH(plpdat)|| ''/'' 
-			|| DAY(plpdat)|| ''/'' || 
-			YEAR(plpdat)					AS ProDate
-		  ,MONTH(plddat)|| ''/'' || 
-			DAY(plddat)|| ''/'' || 
-			YEAR(plddat)					AS dueDate	
+	/*	  ,pbco								AS PoCo		*/
+	/*	  ,pbloc							AS POLoc	*/
+	/*	  ,pbpo#							AS PO#		*/
+	/*	  ,MONTH(phdoi) || ''/''						*/
+	/*		|| DAY(phdoi) || ''/'' ||					*/
+	/*		YEAR(phdoi)						AS IssueDate*/
+	/*	  ,MONTH(plpdat)|| ''/''						*/
+	/*		|| DAY(plpdat)|| ''/'' ||					*/
+	/*		YEAR(plpdat)					AS ProDate	*/
+	/*	  ,MONTH(plddat)|| ''/'' ||						*/
+	/*		DAY(plddat)|| ''/'' ||						*/
+	/*		YEAR(plddat)					AS dueDate	*/
 
 	FROM oohead oh2
 	LEFT JOIN ooline ol2 ON (oh2.ohco = ol2.olco
@@ -58,25 +61,34 @@ BEGIN
 						AND oh2.ohord# = ol2.olord#
 						AND oh2.ohrel# = ol2.olrel#
 						AND oh2.ohcust = ol2.olcust)
-	LEFT JOIN poboline po ON (po.pboco = ol2.olco
-						AND po.pboloc = ol2.olloc
-						AND po.pboord = ol2.olord#
-						AND po.pborel = ol2.olrel#
-						AND po.pbitem = ol2.olitem)
-	LEFT JOIN poline pl ON (pl.plco = po.pbco
-						AND pl.plloc = po.pbloc
-						AND pl.plpo# = po.pbpo#
-						AND pl.plseq# = po.pbseq#
-						AND pl.plitem = po.pbitem)
-	LEFT JOIN pohead ph ON (ph.phco = pl.plco
-						AND ph.phloc = pl.plloc
-						AND ph.phpo# = pl.plpo#)
+/*	LEFT JOIN poboline po ON (po.pboco = ol2.olco		*/
+/*						AND po.pboloc = ol2.olloc		*/
+/*						AND po.pboord = ol2.olord#		*/
+/*						AND po.pborel = ol2.olrel#		*/
+/*						AND po.pbitem = ol2.olitem)		*/
+/*	LEFT JOIN poline pl ON (pl.plco = po.pbco			*/
+/*						AND pl.plloc = po.pbloc			*/
+/*						AND pl.plpo# = po.pbpo#			*/
+/*						AND pl.plseq# = po.pbseq#		*/
+/*						AND pl.plitem = po.pbitem)		*/
+/*	LEFT JOIN pohead ph ON (ph.phco = pl.plco			*/
+/*						AND ph.phloc = pl.plloc			*/
+/*						AND ph.phpo# = pl.plpo#)		*/
 						
 	WHERE oh2.ohord# IN (SELECT Order FROM OO)
-		AND pbpo# IS NOT NULL
+/*			AND pbpo# IS NOT NULL						*/
+		
+		
+	
+	GROUP BY oh2.ohco
+			,oh2.ohord#
+			,oh2.ohuser
+			,ohodat
+	/*		,ohemds			*/							
 		
 	ORDER BY oh2.ohco
 			,oh2.ohuser
+
 	')
 END
 
