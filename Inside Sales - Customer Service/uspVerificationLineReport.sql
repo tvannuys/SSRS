40f352d313cd3fab5e5bfaceaba3a41251c82e77
James Tuttle <jamest@tasupply.com>
2013-12-05 09:10:43 -0800

    SR# 16301

--- Inside Sales - Customer Service/uspVerificationLineReport.sql
+++ Inside Sales - Customer Service/uspVerificationLineReport.sql
@@ -18,27 +18,45 @@ ALTER PROC uspVerificationLineReport AS
 BEGIN
  SELECT *
  FROM OPENQUERY(GSFL2K,	
-	'SELECT oh.ohco				As Comp
-		  ,oh.ohord#						AS OrderNbr
-		  ,oh.ohrel#						AS RelNbr
-		  ,oh.ohuser						AS CreatedBy
-		  ,ohemds							AS SubTotal
-		  ,MONTH(ohodat) || 
-			 ''/'' || DAY(ohodat)|| 
-			 ''/'' || YEAR(ohodat)			AS orderDate
-
-	FROM oohead oh
-	LEFT JOIN ooline ol ON (oh.ohco = ol.olco
+ 	' WITH OO AS (SELECT ohco					AS Co
+					,ohord#						AS Order
+	 /*				,ohrel#						AS Rel		*/			
+					
+					FROM oohead oh
+					LEFT JOIN ooline ol ON (oh.ohco = ol.olco
 						AND oh.ohloc = ol.olloc
 						AND oh.ohord# = ol.olord#
 						AND oh.ohrel# = ol.olrel#
 						AND oh.ohcust = ol.olcust)
+					
+					WHERE ol.olitem IN (''VERIFICATION'',''VERIFICATIONPM'',''VERIFICATIONSAF'',''VERIFICATION80'')
+				  )
+	SELECT oh2.ohco							As Comp
+		  ,oh2.ohord#						AS OrderNbr
+	/*	  ,oh2.ohrel#						AS RelNbr		*/
+		  ,oh2.ohuser						AS CreatedBy
+		  ,SUM(olenet)						AS SubTotal
+		  ,MONTH(ohodat) || 
+			 ''/'' || DAY(ohodat)|| 
+			 ''/'' || YEAR(ohodat)			AS orderDate
+
+	FROM oohead oh2
+	LEFT JOIN ooline ol2 ON (oh2.ohco = ol2.olco
+						AND oh2.ohloc = ol2.olloc
+						AND oh2.ohord# = ol2.olord#
+						AND oh2.ohrel# = ol2.olrel#
+						AND oh2.ohcust = ol2.olcust)
 						
-	WHERE ol.olitem IN (''VERIFICATION'',''VERIFICATIONPM'',''VERIFICATIONSAF'',''VERIFICATION80'')
+	WHERE oh2.ohord# IN (SELECT Order FROM OO)
 	
-	ORDER BY oh.ohco
-			,oh.ohuser
-			,oh.ohord#	
+	GROUP BY  oh2.ohco	
+		  ,oh2.ohord#						
+		  ,oh2.ohuser
+		  ,ohodat
+		  
+	ORDER BY oh2.ohco
+			,oh2.ohuser
+			,oh2.ohord#	
 	')
 END
 
