922bba346fc3ccb0cb2e74b0f12a2cb98b2b8151
James Tuttle <jamest@tasupply.com>
2014-03-24 15:04:56 -0700

    SR# 17979

--- Marketing/uspNewDisplayChargesReport.sql
+++ Marketing/uspNewDisplayChargesReport.sql
@@ -14,13 +14,37 @@
 **																				**
 **********************************************************************************/
 
-CREATE PROC uspNewDisplayChargesReport  AS
-BEGIN
+--CREATE PROC uspNewDisplayChargesReport AS
+ BEGIN
  SELECT *
  FROM OPENQUERY(GSFL2K,	
-	'SELECT *
-		,
-		,
-	FROM Table
+	'SELECT ohord#
+			,MONTH(ohodat) || ''/'' || DAY(ohodat) || ''/'' || YEAR(ohodat) AS Order_Date
+			,ohcust
+			,cmname
+			,olitem
+			,oldesc
+			,'''' AS sellPrice
+			,'' '' AS unitPrice
+			,smno
+			,smname
+
+	FROM oohead oh
+	LEFT JOIN ooline ol ON (ol.olco = oh.ohco
+		    AND ol.olloc = oh.ohloc
+		    AND ol.olord# = oh.ohord#
+		    AND ol.olrel# = oh.ohrel#
+		    AND ol.olcust = oh.ohcust)
+	LEFT JOIN custmast cm ON cm.cmcust = oh.ohcust 
+	LEFT JOIN salesman sm ON sm.smno = ol.olslmn
+	LEFT JOIN itemmast im ON ol.olitem = im.imitem
+
+	WHERE im.imclas = ''DI''
+
 	')
 END
+
+
+
+
+	
