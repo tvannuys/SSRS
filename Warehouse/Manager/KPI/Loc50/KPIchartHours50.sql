5d2ca2c838c715b2a42f67c6bc65ffcfdc8633e0
James Tuttle <jamest@tasupply.com>
2014-04-09 08:44:14 -0700

    SR# 18650

---
+++ Warehouse/Manager/KPI/Loc50/KPIchartHours50.sql
@@ -0,0 +1,61 @@
+USE [GartmanReport]
+GO
+
+/****** Object:  StoredProcedure [dbo].[KPIchartHours]    Script Date: 4/9/2014 8:42:38 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE PROC [dbo].[KPIchartHours50] AS 
+BEGIN
+/*-----------
+SELECT  SUM(CAST(Value  AS decimal)) AS TotalHours
+    
+  FROM [GartmanReport].[dbo].[WarehouseProductivity]
+  WHERE Metric LIKE 'Hour%'
+	AND Location IS NOT NULL
+	AND (RFUser LIKE '[WC]%'
+		OR RFUser LIKE	'[_1]%'
+		OR RFUser LIKE '[_2]%')
+	AND Location = 50 
+	AND CONVERT(varchar(10),RFDate) BETWEEN '01/01/2014' AND '01/31/20114'
+
+
+---------------------------------------------------------------------------------------*/
+--
+-- DETAILS
+--
+
+SELECT  
+--[Company]
+      --,[Location]
+      --,[RFDate]
+      --,[Dept]
+      --,[RFUser]
+      --,[Metric]
+      --,[Daily]
+      ROUND(SUM(CONVERT(float,[Value])),2) AS TotalTime
+  FROM [GartmanReport].[dbo].[WarehouseProductivity]
+  WHERE Metric LIKE 'Hour%'
+	AND Location IS NOT NULL
+	AND (RFUser LIKE '[WC]%'
+		OR RFUser LIKE	'[_1]%'
+		OR RFUser LIKE '[_2]%')
+	AND Location = 50 
+
+--GROUP BY  [Company]
+--      ,[Location]
+--      ,[RFDate]
+--      ,[Dept]
+--      ,[RFUser]
+--      ,[Metric]
+--      ,[Daily]
+	-------------------------------------------------------------------------*/
+	END
+	-- KPIchartHours
+GO
+
+
