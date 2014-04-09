07304c7262e7b3107e40318ca5d64948ffd22b07
James Tuttle <jamest@tasupply.com>
2014-04-09 07:59:17 -0700

    SR# 18650

---
+++ Warehouse/Manager/KPI/KPIchart_InvAdj50.sql
@@ -0,0 +1,76 @@
+USE [GartmanReport]
+GO
+
+/****** Object:  StoredProcedure [dbo].[KPIchart_InvAdj50]    Script Date: 4/9/2014 7:45:32 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE PROC [dbo].[KPIchart_InvAdj50] AS
+BEGIN	
+--====================================================================================================================
+--
+-- C for Current time period =========================================================================================
+--
+
+/* DROP a temp table if exists */
+	IF EXISTS(SELECT * FROM tempdb.dbo.sysobjects WHERE ID = OBJECT_ID (N'tempdb..#WCC_C'))
+		BEGIN
+			DROP TABLE #WCC_C
+		END;
+	WITH CTE_C AS	-- Current year and month
+	(
+	SELECT SUM(TotalCost) AS CntC
+	FROM InventoryAdjustmentsWH
+	WHERE CONVERT(date,[Date]) BETWEEN '01/01/2014' AND '01/31/2014' 
+		AND Loc = 50 )
+
+	SELECT CntC as C_cnt, NULL as P_cnt
+	INTO #WCC_C
+
+	FROM CTE_C
+--==================================================================================================================
+--
+-- P for Prior time period =========================================================================================
+--
+/* DROP a temp table if exists */
+	IF EXISTS(SELECT * FROM tempdb.dbo.sysobjects WHERE ID = OBJECT_ID (N'tempdb..#WCC_P'))
+		BEGIN
+			DROP TABLE #WCC_P
+		END;	
+
+	WITH CTE_P AS
+	(SELECT SUM(TotalCost) AS CntP
+	FROM InventoryAdjustmentsWH
+	WHERE CONVERT(date,[Date]) BETWEEN '01/01/2013' AND '01/31/2013' 
+		AND Loc = 50 )
+
+	SELECT CntP as P_cnt, NULL as C_cnt
+	INTO #WCC_P
+
+	FROM CTE_P
+
+
+--==================================================================================================================
+-- UPSERT 
+--==================================================================================================================
+	MERGE #WCC_C as T1			-- table one with the Current time fram
+	USING #WCC_P as T2			-- table two with the Prior time frame feeding table 1 with UPDATE or INSERT
+	ON T1.C_cnt = T2.P_cnt 		-- alias the two tables 
+	WHEN MATCHED THEN			-- if a match on xxxxx UPDATE fields from table 2 feed
+		UPDATE SET T1.C_cnt = T2.P_cnt 
+	WHEN NOT MATCHED THEN		-- if xxxxx not present INSERT the fields from table 2 feed
+		INSERT ( P_cnt)
+		VALUES ( T2.P_cnt);
+		
+		
+	SELECT * FROM #WCC_C
+
+END
+
+GO
+
+
---
+++ Warehouse/Manager/KPI/KPIchart_WCC_Cust_CNT_loc50.sql
@@ -0,0 +1,102 @@
+
+-------------------------------------------------------------
+CREATE PROC KPIchart_WCC_Cust_CNT_loc50 AS
+BEGIN
+---------------------------------
+--
+--	C = current time period
+--
+---------------------------------
+/* DROP a temp table if exists */
+	IF EXISTS(SELECT * FROM tempdb.dbo.sysobjects WHERE ID = OBJECT_ID (N'tempdb..#WCC_C'))
+		BEGIN
+			DROP TABLE #WCC_C
+		END;
+-- Collect status = 'T' SHIPPED
+	WITH CTE_C AS
+	(
+	SELECT *
+	FROM OPENQUERY (GSFL2K,'SELECT rfcust 
+								FROM rfwillchst
+								WHERE rfloc =50
+									AND rfodate BETWEEN ''01/01/2014'' AND ''01/31/2014'' 
+									AND rpstat = ''T''
+									AND rfobin# != ''SHIPD''
+			
+							')
+							)
+	SELECT COUNT(DISTINCT rfcust) as c_cnt, NULL as p_cnt
+	INTO #WCC_C
+
+	FROM CTE_C
+--==================================================================================================================
+---------------------------------
+--
+--	P = prior time period
+--
+---------------------------------
+/* DROP a temp table if exists */
+	IF EXISTS(SELECT * FROM tempdb.dbo.sysobjects WHERE ID = OBJECT_ID (N'tempdb..#WCC_P'))
+		BEGIN
+			DROP TABLE #WCC_P
+		END;	
+-- Collect status = 'T' SHIPPED
+	WITH CTE_P AS
+	(
+	SELECT *
+	FROM OPENQUERY (GSFL2K,'SELECT rfcust
+								FROM rfwillchst
+								WHERE rfloc =50
+									AND rfodate BETWEEN ''01/01/2013'' AND ''01/31/2013'' 
+									AND rpstat = ''T''
+									AND rfobin# != ''SHIPD''
+			
+							')
+							)
+	SELECT COUNT(DISTINCT rfcust) as p_cnt, NULL as c_cnt
+	INTO #WCC_P
+
+	FROM CTE_P
+
+--==================================================================================================================
+-- UPSERT 
+--==================================================================================================================
+	MERGE #WCC_C as T1			-- table one with the C Current time period
+	USING #WCC_P as T2			-- table two with the P for prior time period feeding table 1 with UPDATE or INSERT
+	ON T1.c_cnt = T2.p_cnt		-- alias the two tables 
+	WHEN MATCHED THEN			-- if a match on hour UPDATE fields from table 2 feed
+		UPDATE SET T1.c_cnt = T2.c_cnt
+	WHEN NOT MATCHED THEN		-- if hour not present INSERT the fields from table 2 feed
+		INSERT ( p_cnt)
+		VALUES (T2.p_cnt);
+		
+		
+	SELECT * FROM #WCC_C
+		
+END	
+
+GO
+
+
+
+
+
+
+
+
+
+
+
+/*-------------------------------------------------------------
+------- DETAILS -----------------------------------------------
+-
+	SELECT *
+	FROM OPENQUERY (GSFL2K,'SELECT *
+								FROM rfwillchst 
+								WHERE rfloc =50
+									AND rfodate = CURRENT_DATE
+									AND rpstat = ''T''
+									AND rfobin# != ''SHIPD''
+			
+							')
+-------------------------------------------------------------*/
