395617bd7f7172026c71da94f4279a11e323d943
James Tuttle <jamest@tasupply.com>
2014-04-09 15:55:12 -0700

    SR# 18650

---
+++ Warehouse/Manager/KPI/Loc52/KPIchart_AVG_Time_Prior_loc52.sql
@@ -0,0 +1,141 @@
+USE [GartmanReport]
+GO
+
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE PROC [dbo].[KPIchart_AVG_Time_Prior_loc52] AS 
+	
+---------------------------------------------------------------------------------------------
+--===========================================================================================	
+--
+-- GET AVERAGE TIME PULLED FOR THE MONTH	
+--
+--===========================================================================================
+---------------------------------------------------------------------------------------------
+---------------------------------
+--
+--	C = current time period
+--
+---------------------------------
+	
+ WITH CTEtime AS
+ (   
+SELECT rfco
+	  ,rfloc
+	  ,rfcust
+	  ,CAST(rfotime as INT) as rfotime
+	  ,CAST(rptime as INT) as rptime
+	FROM OPENQUERY (GSFL2K,'SELECT rfco
+								,rfloc
+								,rfcust
+							/*	,rfoord#
+								,rforel#	*/
+								,rfotime
+								,rptime
+							FROM rfwillchst
+							WHERE rfloc = 52
+								AND rfodate BETWEEN ''01/01/2013'' AND ''01/31/2013'' 
+								AND rpstat = ''T''
+								AND rfobin# != ''SHIPD''
+					')
+GROUP BY rfco
+		,rfloc
+		,rfcust
+		--,rfoord#
+		--,rforel#
+		,rfotime
+		,rptime
+--ORDER BY rfcust
+--		,rfotime
+)		
+
+SELECT    CONVERT(CHAR(20), DATEADD(SECOND, AVG(DATEDIFF(SECOND,
+          CONVERT(DATETIME,CONVERT(VarCHAR(4),rfotime/10000)+':'
+         +CONVERT(VARCHAR(4),(rfotime/100)%100)+':'
+         +CONVERT(VARCHAR(4),rfotime%100),8),
+          CONVERT(DATETIME,CONVERT(VarCHAR(02),rptime/10000)+':'
+         +CONVERT(VARCHAR(4),(rptime/100)%100)+':'
+         +CONVERT(VARCHAR(4),rptime%100),8))),
+          CONVERT(DATETIME, '00:00:00', 113)),8)                  
+     AS ElapsedTime
+FROM CTEtime
+
+/*
+---------------------------------------------------------------------------------------------------------------------------
+-- KEEP FOR DETAILS REPORTING
+---------------------------------------------------------------------------------------------------------------------------
+
+
+SELECT *
+, CONVERT(varchar(8),cast(
+    cast(
+       substring(right('000000'+cast(rptime as varchar),6),1,2)
+       + ':' +
+       substring(right('000000'+cast(rptime as varchar),6),3,2)
+       + ':' +
+       substring(right('000000'+cast(rptime as varchar),6),5,2) 
+    as datetime) - --Do the math on the rptime(End) and rfotime(start) time to get the elapsed time
+    cast(
+       substring(right('000000'+cast(rfotime as varchar),6),1,2)
+       + ':' +
+       substring(right('000000'+cast(rfotime as varchar),6),3,2)
+       + ':' +
+       substring(right('000000'+cast(rfotime as varchar),6),5,2) 
+    as datetime)
+  as time)) as ElapsedTime
+	FROM OPENQUERY (GSFL2K,'SELECT rfco
+								,rfloc
+								,rfcust
+							/*	,rfoord#
+								,rforel#	*/
+								,rfotime
+								,rptime
+							FROM rfwillchst
+							WHERE rfloc = 52
+								AND rfodate = CURRENT_DATE - 3 days
+								AND rpstat = ''T''
+								AND rfobin# != ''SHIPD''
+					')
+GROUP BY rfco
+		,rfloc
+		,rfcust
+		--,rfoord#
+		--,rforel#
+		,rfotime
+		,rptime
+ORDER BY rfcust
+		,rfotime
+		
+-------------------------------------------------------------------------------------------------------------------------------			
+		
+SELECT *
+	FROM OPENQUERY (GSFL2K,'SELECT rfco
+								,rfloc
+								,rfcust
+							 	,rfoord#
+								,rforel#	
+								,rfotime
+								,rptime
+							FROM rfwillchst
+							WHERE rfloc = 52
+								AND rfodate = CURRENT_DATE - 3 days
+								AND rpstat = ''T''
+								AND rfobin# != ''SHIPD''
+							--	AND rfoord# = 34804
+					')
+ORDER BY rfcust
+		,rfotime
+*/
+-----------------------------------------------------------------------------------------------------------------------------------
+
+GO
+
+
+
+
---
+++ Warehouse/Manager/KPI/Loc52/KPIchart_Irr52.sql
@@ -0,0 +1,76 @@
+USE [GartmanReport]
+GO
+
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE PROC [dbo].[KPIchart_Irr52] AS
+BEGIN	
+--====================================================================================================================
+--
+-- C for Current time period =========================================================================================
+--
+
+/* DROP a temp table if exists */
+	IF EXISTS(SELECT * FROM tempdb.dbo.sysobjects WHERE ID = OBJECT_ID (N'tempdb..#WCC_C'))
+		BEGIN
+			DROP TABLE #WCC_C
+		END;
+	WITH CTE_C AS	-- Current year and month
+	(
+	SELECT COUNT([Root Cause]) AS CntC
+	FROM IRRReportData
+	WHERE OrderDate BETWEEN '01/01/2014' AND '01/31/2014' 
+		AND ErrorLocation = 52)
+
+	SELECT CntC as C_cnt, NULL as P_cnt
+	INTO #WCC_C
+
+	FROM CTE_C
+--==================================================================================================================
+--
+-- P for Prior time period =========================================================================================
+--
+/* DROP a temp table if exists */
+	IF EXISTS(SELECT * FROM tempdb.dbo.sysobjects WHERE ID = OBJECT_ID (N'tempdb..#WCC_P'))
+		BEGIN
+			DROP TABLE #WCC_P
+		END;	
+
+	WITH CTE_P AS
+	(SELECT COUNT([Root Cause]) AS CntP
+	FROM IRRReportData
+	WHERE OrderDate BETWEEN '01/01/2013' AND '01/31/2013'
+		AND ErrorLocation = 52 )
+
+	SELECT CntP as P_cnt, NULL as C_cnt
+	INTO #WCC_P
+
+	FROM CTE_P
+
+
+--==================================================================================================================
+-- UPSERT 
+--==================================================================================================================
+	MERGE #WCC_C as T1			-- table one with the Current time fram
+	USING #WCC_P as T2			-- table two with the Prior time frame feeding table 1 with UPDATE or INSERT
+	ON T1.C_cnt = T2.P_cnt 		-- alias the two tables 
+	WHEN MATCHED THEN			-- if a match on xxxxx UPDATE fields from table 2 feed
+		UPDATE SET T1.C_cnt = T2.P_cnt 
+	WHEN NOT MATCHED THEN		-- if xxxxx not present INSERT the fields from table 2 feed
+		INSERT ( P_cnt)
+		VALUES ( T2.P_cnt);
+		
+		
+	SELECT * FROM #WCC_C
+
+END
+-- SELECT * FROM IRRReportData
+GO
+
+
