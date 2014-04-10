23ec1c41914d56b2001ec783d4c1cf485bf7e51c
James Tuttle <jamest@tasupply.com>
2014-04-10 14:07:22 -0700

    Fix table call

--- Warehouse/Manager/KPI/Loc4/KPIchart_InvAdj4.sql
+++ Warehouse/Manager/KPI/Loc4/KPIchart_InvAdj4.sql
@@ -9,7 +9,7 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 
-CREATE PROC [dbo].[KPIchart_InvAdj4] AS
+ALTER PROC [dbo].[KPIchart_InvAdj4] AS
 BEGIN	
 --====================================================================================================================
 --
@@ -24,7 +24,7 @@ BEGIN
 	WITH CTE_C AS	-- Current year and month
 	(
 	SELECT SUM(TotalCost) AS CntC
-	FROM InventoryAdjustmentsWH
+	FROM InventoryAdjustmentsWH_KPI
 	WHERE CONVERT(date,[Date]) BETWEEN '01/01/2014' AND '01/31/2014' 
 		AND Loc IN (4,44,64) )
 
@@ -44,7 +44,7 @@ BEGIN
 
 	WITH CTE_P AS
 	(SELECT SUM(TotalCost) AS CntP
-	FROM InventoryAdjustmentsWH
+	FROM InventoryAdjustmentsWH_KPI
 	WHERE CONVERT(date,[Date]) BETWEEN '01/01/2013' AND '01/31/2013' 
 		AND Loc IN (4,44,64) )
 
