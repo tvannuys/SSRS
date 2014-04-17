c667ace239881d6b6117d31c5d8cf8cbfaab2b2e
James Tuttle <jamest@tasupply.com>
2014-04-17 08:28:28 -0700

    SR# 18650

---
+++ Warehouse/Manager/KPI/Loc41/Details/KPIchart_InvAdj41_PriorDetails41.sql
@@ -0,0 +1,7 @@
+
+
+ALTER PROC	[KPIchart_InvAdj41_PriorDetails41]	AS
+SELECT *
+	FROM InventoryAdjustmentsWH_KPI
+	WHERE CONVERT(date,[Date]) BETWEEN '01/01/2013' AND '01/31/2013' 
+		AND Loc IN (52,41,57)
