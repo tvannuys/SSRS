43160a83fc121f524f2ab6259cffe364497f32b5
gwrippee <georger@tasupply.com>
2014-04-17 09:07:46 -0700

    Made changes per Rhonda's request to break out mfg rebate and buying group rebate.

--- Finance/Audit Support/Sales Records.sql
+++ Finance/Audit Support/Sales Records.sql
@@ -1,49 +1,43 @@
-/* 
-
-Order Type FN - finance user
-SHUSER = ARMDATA - armstrong transactions prior to acquisition
-
-*/
-
 select * from openquery (gsfl2k,'
 
-SELECT      SHHEAD.SHINV# AS InvoiceNbr, 
-            SHHEAD.SHIDAT AS InvoiceDate, 
-            SHHEAD.SHLOC AS Location, 
-            SHHEAD.SHVIAC AS ShipViaCode,
-            SHHEAD.SHVIA AS ShipViaDesc,
-            SHHEAD.SHCUST AS CustNbr, 
-            CUSTMAST.CMNAME AS CustName, 
-            CUSTMAST.CMADR1 as BillingAddr1, 
-            CUSTMAST.CMADR2 as BillingAddr2,
-            Left(CMADR3,23) AS BillingCity, 
-            Right(CMADR3,2) AS BillingState,  
-            CUSTMAST.CMZIP as BillingZip, 
-            SHHEAD.SHSTA1 AS ShipToAddr1, 
-            SHHEAD.SHSTA2 AS ShipToAddr2, 
-            Left(SHSTA3,23) AS ShipToCity, 
-            Right(SHSTA3,2) AS ShipToState, 
-            SHHEAD.SHZIP AS ShipToZipCode, 
-            SHHEAD.SHEMDS AS Subtotal,
-            SHSPC1 AS DeliveryCharge,
-            SHSPC2 AS Restock,
-            SHSPC4 AS FreightHandling,
-            SHSPC5 AS FuelSurcharge, 
-            SHHEAD.SHESTA AS StateTaxAmt, 
-            SHHEAD.SHECNA AS CountyTaxAmt, 
-            SHHEAD.SHECIA AS CityTaxAmt, 
-            SHHEAD.SHTOTL AS InvoiceTotal,
-            shhead.shuser as User,
-            shhead.shotyp as OrderType
+select	slinv# as InvoiceNum,
+		sldate as InvoiceDate,
+		shodat as OrderDate,
+		shotyp as OrderType,
+		slco as Co,
+		slloc as Loc,
+		slcust as CustNum,
+		cmname as CustName,
+		right(cmadr3,2) as State,
+		slitem as ItemNum,
+		sldesc as ItemDesc,
+		slprcd as ProdCode,
+		slfmcd as FamilyCode,
+		slvend as VendNum,
+		slqshp as ShipQtyInvUnit,
+		slblus as ShipQtyBillUnit,
+		slpor as PriceOverride,
+		slpric as UnitPrice,
+		slscs4 as UnitFileback,
+		slscs5 as UnitBuyingGroup,
+		sleprc as ExtendedPrice,
+		slecst as ExtendedCost,
+		slesc4 as ExtendedFileback,
+		slesc5 as ExtendedBuyingGroup,
+		slgl# as SalesGL,
+		slinvgl as InventoryGL,
+		slcogsgl as COGSGL,
+		shuser
+		
+from shline sl
+	left join shhead sh on (sh.shco=sl.slco and sh.shloc=sl.slloc and sh.shord#=sl.slord# and sh.shrel#=sl.slrel# and sh.shinv#=sl.slinv# and sh.shidat=sl.sldate)
+	left join custmast cm on cm.cmcust = sl.slcust
 
-FROM SHHEAD 
-      INNER JOIN CUSTMAST ON SHHEAD.SHCUST = CUSTMAST.CMCUST
-
-WHERE SHHEAD.SHIDAT Between ''01/01/2011'' And ''12/31/2011'' 
-      AND SHHEAD.SHCO = 2
-      
-ORDER BY SHHEAD.SHIDAT
-
-
-
+where slco=2
+	and sldate between ''01/01/2011'' and ''12/31/2011''
+	and shuser <> ''ARMDATA''
+	
+	
+order by sldate asc
+	
 ')
