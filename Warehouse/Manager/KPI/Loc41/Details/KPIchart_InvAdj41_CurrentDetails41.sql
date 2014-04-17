bbcfab2ed0db988856a34d7842d130b024367aaf
James Tuttle <jamest@tasupply.com>
2014-04-17 08:54:33 -0700

    KPI

--- Warehouse/Manager/KPI/Loc41/Details/KPIchart_InvAdj41_CurrentDetails41.sql
+++ Warehouse/Manager/KPI/Loc41/Details/KPIchart_InvAdj41_CurrentDetails41.sql
@@ -1,7 +1,7 @@
 
 
-CREATE PROC	[KPIchart_InvAdj41_PriorDetails41]	AS
+CREATE PROC	[KPIchart_InvAdj41_CurrentDetails41]	AS
 SELECT *
 	FROM InventoryAdjustmentsWH_KPI
-	WHERE CONVERT(date,[Date]) BETWEEN '01/01/2013' AND '01/31/2013' 
+	WHERE CONVERT(date,[Date]) BETWEEN '01/01/2014' AND '01/31/2014' 
 		AND Loc IN (52,41,57)
