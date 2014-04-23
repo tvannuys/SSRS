a1a5ac8b16112bce77babbf5d631b7a60b08aefc
tvannuys <thomasv@tasupply.com>
2014-04-23 11:00:42 -0700

    Simplified after investigating schema and actual fields in use

--- Utility/spFindEmail.sql
+++ Utility/spFindEmail.sql
@@ -1,22 +1,6 @@
-USE [GartmanReport]
-GO
+-- spFindEmail 'aqui'
 
-/****** Object:  StoredProcedure [dbo].[spFindEmail]    Script Date: 04/22/2014 12:51:23 ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-/* Find contacts in Gartman that have a particular email address
-
-	Like condition search
-
-spFindEmail thomasv
-	
-*/
-
-ALTER proc [dbo].[spFindEmail] 
+alter proc [dbo].[spFindEmail] 
 
 @AddrPortion varchar(30)
 
@@ -25,44 +9,22 @@ as
 declare @sql varchar(max)
 
 set @sql = 'select * from openquery(gsfl2k,''
+select cmcust as Acct,
+cnccontid as ContactID,
+cncfname as ContactFirstName,
+cnclname as ContactLastName,
+cnce_Mail as ContactEmail,
+ccde_mail as ContactDetailEmail,
+ccddoc as ContactDocument
 
-SELECT CUSTMAST.CMCO AS Company,
-CUSTMAST.CMCUST AS CustNbr, 
-CUSTMAST.CMNAME AS CustName, 
-CUSTMAST.CMADR1 AS Addr1, 
-CUSTMAST.CMADR2 AS Addr2, 
-Left(CMADR3,23) AS City, 
-Right(CMADR3,2) AS State, 
-CUSTMAST.CMZIP AS ZipCode, 
-CUSTMAST.CMPHON AS Phone, 
-CUSTMAST.CMFAX AS FaxNbr, 
-CUSTMAST.CMSLMN AS SalesmanNbr, 
-SALESMAN.SMNAME AS RepName, 
-CUSTEXTN.CEXCLASS AS CustTypeCode, 
-CUCLMAST.CCLDESC AS CustTypeDesc, 
-CCDCONTID as ContactID,
-custcndtl.CCDCNT AS ContactName, 
-custcndtl.CCDE_MAIL AS "PriceListE-MailAddr",
-CCDDOC as Document
+from custmast c
 
-FROM  CUSTMAST 
+left join CONTXREF cm on c.cmcust = cm.ccnxacct
+left join contcont ct on ct.cnccontid = cm.ccnxcontid
+left join custcndtl cd on cd.ccdcontid = ct.cnccontid
 
-LEFT JOIN SALESMAN ON CUSTMAST.CMSLMN = SALESMAN.SMNO
-LEFT JOIN CUSTXTRA ON CUSTMAST.CMCUST = CUSTXTRA.CXCUST 
-LEFT JOIN CUSTEXTN ON CUSTMAST.CMCUST = CUSTEXTN.CEXCUST
-left JOIN CUCLMAST ON CUCLMAST.CCLCLASS = CUSTEXTN.CEXCLASS
-LEFT JOIN custcndtl ON (CUSTMAST.CMCUST = custcndtl.CCDCUS)  
-
-WHERE CUSTEXTN.CEXCLASS <> ''''999'''' 
-AND CUSTMAST.CMDELT <> ''''H''''
-
-and ccde_mail is not null
-and upper(ccde_mail) like ''''%' + upper(@AddrPortion) + '%'''' 
-
+where (upper(ccde_mail) like ''''%' + upper(@AddrPortion) + '%''''
+		or upper(cnce_mail) like ''''%' + upper(@AddrPortion) + '%'''')
 '')'
 
 exec(@sql)
-
-GO
-
-
