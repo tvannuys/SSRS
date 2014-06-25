5fd1af18867abec9d5a7c19331fd5d9fbbe3057e
Thomas Van Nuys <thomasv@tasupply.com>
2014-06-25 09:17:03 -0700

    SR 22634 - use IM class instead of vendor criteria for source data of report

--- Inventory/InboundFrieghtPM.sql
+++ Inventory/InboundFrieghtPM.sql
@@ -1,20 +1,35 @@
+USE [GartmanReport]
+GO
+
+/****** Object:  StoredProcedure [dbo].[InboundFrieghtPM]    Script Date: 06/25/2014 09:03:20 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
 /*----------------------------------------------------------
 --
 -- Jame Tuttle	09/25/2012
 --
 -- Added not to include reverse POs
--- PHRETURN != ''Y''
+-- PHRETURN != 'Y'
+
+
+-- Thomas 6/25/14
+removed code below in favor of and itemmast.imclas = ''IM''
+
+and ((poline.plvend in (22666,16088,22816,22887,22204,22949,22686,22674,22859,24293)) or
+	 (poline.plvend = 22674 and imfmcd = ''LV'') or
+	 (poline.plvend = 24005 and imfmcd in (''L0'',''LV'')) or
+	 (poline.plvend = 16006 and imfmcd = ''V4'' and imsi = ''Y'') or
+	 (poline.plvend = 22312 and imfmcd = ''YH'' and imsi = ''Y'')
+	)
+
 ------------------------------------------------------------*/
--- SR# 8370
--- James Tuttle 05/07/2013
---  Rename a couple of columns
---	Rearrange the display of fields
---  Add columns:	@ Not Attached
---					@ Avail
-----------------------------------------------------------------
 
 
-ALTER Proc [dbo].[InboundFrieghtPM] as
+alter Proc [dbo].[InboundFrieghtPM] as
 
 select OQ.*, MT.MTSTATUS
 from openquery(GSFL2K,'
@@ -29,6 +44,7 @@ Poline.PLPO# As PO,
 Poline.PLITEM As SKU, 
 Poline.PLDESC As Description, 
 Itemmast.IMCOLR As Color, 
+Poline.PLQORD As UnitsOrder,
 Itemfact.IFFACA, 
 Itemfact.IFUMC, 
 Poline.PLQORD/Itemfact.IFFACA As TotalPallets,
@@ -36,12 +52,7 @@ Poline.PLQORD/Itemfact.IFFACA As TotalPallets,
 						and mnpolo = poline.plloc
 						and mnitem = poline.plitem
 						and mnpoco = poline.plco) as Manifest,
-(select sum(ibqoh) from itembal where ibitem = itemmast.imitem) as OnHand,
-(select sum(ibqoh) - sum(ibqoo) from itembal where ibitem = itemmast.imitem) as Avail,
-
-sum(Poline.PLQORD) As UnitsOrder,
-sum(ifnull(Poboline.PBOQTY,0)) As QtySold,
-sum(Poline.PLQORD - ifnull(Poboline.PBOQTY,0)) as NotAttached 
+(select sum(ibqoh) from itembal where ibitem = itemmast.imitem) as OnHand
 
 FROM Poline
 
@@ -53,10 +64,6 @@ Left Join Pohead On (Poline.PLco = Pohead.PHco
 Left Join Itemfact On Poline.PLITEM = Itemfact.IFITEM 
 Left Join Vendmast On Poline.PLVEND = Vendmast.VMVEND 
 Left Join Family On Poline.PLFMCD = Family.FMFMCD 
-Left Join Poboline On (Poline.plco = Poboline.pbco
-					AND Poline.plloc = Poboline.pbloc
-					AND poline.plpo# = Poboline.pbpo#
-					AND Poline.plitem = Poboline.pbitem)
 
 Where Itemfact.IFUMC = ''P'' 
 AND Poline.PLITEM <> ''GENPROMO'' 
@@ -67,67 +74,22 @@ AND Poline.PLCO = 2
 and poline.plloc in (41)
 AND Poline.PLDELT <> ''C''
 AND Poline.PLDIRS <> ''Y'' 
-and ((poline.plvend in (22666,16088,22816,22887,22204,22949,22686,22674,22859)) or
-	 (poline.plvend = 22674 and imfmcd = ''LV'') or
-	 (poline.plvend = 24005 and imfmcd in (''L0'',''LV'')) or
-	 (poline.plvend = 16006 and imfmcd = ''V4'' and imsi = ''Y'') or
-	 (poline.plvend = 22312 and imfmcd = ''YH'' and imsi = ''Y'')
-	)
+and itemmast.imclas = ''IM''
 and Poline.PLDDAT <> ''0001-01-01''
 AND POhead.phreturn != ''Y''
-
-
- GROUP BY Poline.PLDDAT
-,Poline.PLCO
-,Poline.PLLOC 
-,Poline.PLDDAT
-,Family.FMDESC
-,Vendmast.VMNAME
-,Poline.PLPO#
-,Poline.PLITEM  
-,Poline.PLDESC
-,Itemmast.IMCOLR
-,Itemfact.IFFACA
-,Itemfact.IFUMC
-,Poline.PLDDAT - 7 DAYS
-,Poline.PLQORD/Itemfact.IFFACA
-,(select max(mnman#) from manifest where mnpo# = poline.plpo#
-						and mnpolo = poline.plloc
-						and mnitem = poline.plitem
-						and mnpoco = poline.plco)
-,(select sum(ibqoh) from itembal where ibitem = itemmast.imitem) 
-,(select sum(ibqoh) - sum(ibqoo) from itembal where ibitem = itemmast.imitem)
 
 Order By Poline.PLDDAT, Vendmast.VMNAME, Poline.PLPO#, Poline.PLITEM 
 ') OQ
 
 LEFT join GSFL2K.B107Fd6E.GSFL2K.MANTRACK MT on Manifest = MT.MTMAN#
 
- 
 
---DUEDATE
---		,ACTUALarrivalDUEdate
---		,PLCO
---		,LOC
---		,PO
---		,DUUEDATE
---		,FMDESC
---		,VENDORNAME
---		,SKU
---		,[DESCRIPTION]
---		,COLOR
---		,UNITSORDER
---		,QTYSOLD
---		,NOTATTACHED
---		,IFFACA
---		,IFUMC
---		,TOTALPALLETS
---		,MANIFEST
---		,ONHAND
---		,AVAIL
---		,MTSTATUS
+
+
+
+
+
+
 GO
 
 
-/*
-*/
