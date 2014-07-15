fdc4b8b69d44a7d7bfaf933602d15b473411c1c9
Thomas Van Nuys <thomasv@tasupply.com>
2014-07-15 13:10:31 -0700

    updating file in Inventory directory with proper changes

--- Inventory/InboundFrieghtTA.sql
+++ Inventory/InboundFrieghtTA.sql
@@ -1,13 +1,3 @@
-USE [GartmanReport]
-GO
-
-/****** Object:  StoredProcedure [dbo].[InboundFrieghtTA]    Script Date: 10/15/2013 11:41:34 ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
 
 /*----------------------------------------------------------
 --
@@ -15,17 +5,11 @@ GO
 --
 -- Added not to include reverse POs
 -- PHRETURN != 'Y'
-
-
--- Thomas  10/15/2013
---
--- Changed item criteria to use IM clas indicator
-
 ------------------------------------------------------------*/
 
 
 
-alter Proc [dbo].[InboundFrieghtTA] as
+ALTER Proc [dbo].[InboundFrieghtTA] as
 
 select OQ.*,MT.MTSTATUS
 from openquery(GSFL2K,'
@@ -70,19 +54,17 @@ AND Poline.PLCO = 1
 and poline.plloc not in (41,42,60)
 AND Poline.PLDELT <> ''C''
 AND Poline.PLDIRS <> ''Y'' 
-and itemmast.imclas = ''IM''
-/*
 and ((poline.plvend in (22666,16088,22816,22887,22204,22949,22686,22674,22731,22859,23306)) or
 	 (poline.plvend = 21861 and imprcd in (34057,34058)))
-	 
-*/
 and Poline.PLDDAT <> ''0001-01-01''
 AND POhead.phreturn != ''Y''
 
 Order By Poline.PLDDAT, Vendmast.VMNAME, Poline.PLPO#, Poline.PLITEM 
 ') OQ
 
-LEFT join (select * from openquery(GSFL2K,'select * from MANTRACK')) MT on OQ.Manifest = MT.MTMAN#
+LEFT join GSFL2K.B107Fd6E.GSFL2K.MANTRACK MT on Manifest = MT.MTMAN#
+
+
 
 
 GO
