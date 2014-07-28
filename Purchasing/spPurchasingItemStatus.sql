d53716d153182c6f296ba1064abd4fd298da7c4e
Thomas Van Nuys <thomasv@tasupply.com>
2014-07-28 10:23:59 -0700

    SR 23712

--- Purchasing/spPurchasingItemStatus.sql
+++ Purchasing/spPurchasingItemStatus.sql
@@ -1,14 +1,21 @@
+/*
+
+7-28-14 - SR# 23712 - exclude transfers from On Order total, line below removed
+	sum(IBQOOV) as QtyOnOrder,
+	
+*/
+
 USE [GartmanReport]
 GO
 
-/****** Object:  StoredProcedure [dbo].[spPurchasingItemStatus]    Script Date: 07/28/2014 10:14:25 ******/
+/****** Object:  StoredProcedure [dbo].[spPurchasingItemStatus]    Script Date: 07/28/2014 10:01:02 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE proc [dbo].[spPurchasingItemStatus] 
+ALTER proc [dbo].[spPurchasingItemStatus] 
 
 @Item varchar(30)
 
@@ -17,9 +24,12 @@ as
 declare @sql varchar(1000)
 
 set @sql = 'select * from openquery(gsfl2k,''
-select ibitem,imdesc,imcolr,sum(ibqoh) as OnHand,
+select ibitem,
+imdesc,
+imcolr,
+sum(ibqoh) as OnHand,
 
-sum(IBQOOV) as QtyOnOrder,
+sum(IBQOOV) - (select sum(OLQORD) from ooline where OLOTYP <> ''TR'' and OLITEM = ibitem) as QtyOnOrder,
 sum(IBQOH-IBQOO) AS QtyAvailable
 
 from itembal
