f15c949cacbe760ce452f7584cf535991c6afbc2
Thomas Van Nuys <thomasv@tasupply.com>
2014-08-05 15:11:56 -0700

    Initial RDL build - query refined and turned into stored proc

--- Purchasing/COGS based Forcasting SR 23970.sql
+++ Purchasing/COGS based Forcasting SR 23970.sql
@@ -5,17 +5,28 @@ SR 23970
 
 Created:  8/4/2014 - Thomas
 
+where SLSDAT >= CURDATE() - 90 days
+
+sum(SLECST+SLESC1+SLESC2+SLESC3+SLESC4+SLESC5) as SOCOGS,
+
+-- want COGS for last 3 complete months
  
 */
+alter proc spCOGSForecast 
 
+@StartDate as varchar(15), 
+@EndDate as varchar(15) 
 
-select *
+as
 
-from openquery(gsfl2k,'
+declare @sql as varchar(max)
+
+set @sql = 'select *
+from openquery(gsfl2k,''
 select i1.imitem,
 i1.imdesc,
 i1.imcolr,
-sum(SLECST) as SOCOGS,
+sum(SLECST+SLESC1+SLESC2+SLESC3+SLESC4+SLESC5) as SOCOGS,
 ifnull((select sum(PBOQTY*i2.IMCOST)
 		from poboline 
 		join itemmast i2 on i2.imitem = pbitem
@@ -24,13 +35,16 @@ ifnull((select sum(PBOQTY*i2.IMCOST)
 from shline
 LEFT JOIN ITEMMAST i1 ON SHLINE.SLITEM = i1.IMITEM 
 		
-where SLSDAT >= CURDATE() - 90 days
-and i1.IMCLAS = ''IM''
+where SLDATE >= ''''' + @StartDate + '''''
+ and SLDATE <= ''''' + @EndDate + '''''
+ and i1.IMCLAS = ''''IM''''
 
 group by i1.imitem,
 i1.imdesc,
 i1.imcolr
 
-')
+'')'
+
+exec(@sql)
 
 
