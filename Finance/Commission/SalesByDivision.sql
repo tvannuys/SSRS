03feab106a9b38325a94c8ea0884016bfd0e95a5
Thomas Van Nuys <thomasv@tasupply.com>
2014-09-03 07:25:39 -0700

    Commission run for August

--- Finance/Commission/SalesByDivision.sql
+++ Finance/Commission/SalesByDivision.sql
@@ -16,7 +16,7 @@ declare @MSsql varchar(max)
 declare @ReportMonth varchar(2)
 declare @ReportYear varchar(4)
 
-set @ReportMonth = '7'  
+set @ReportMonth = '8'  
 set @ReportYear = '2014'  -- use 4 digit year
 
 
