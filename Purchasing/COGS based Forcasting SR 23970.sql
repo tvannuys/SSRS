fb255c5f6ffe1496424324a5e75f345792c6ea98
Thomas Van Nuys <thomasv@tasupply.com>
2014-09-03 18:11:08 -0700

    correction to YTD sales

--- Purchasing/COGS based Forcasting SR 23970.sql
+++ Purchasing/COGS based Forcasting SR 23970.sql
@@ -1,12 +1,13 @@
 USE [GartmanReport]
 GO
 
-/****** Object:  StoredProcedure [dbo].[spCOGSForecast]    Script Date: 08/06/2014 09:38:41 ******/
+/****** Object:  StoredProcedure [dbo].[spCOGSForecast]    Script Date: 09/03/2014 13:08:18 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
+
 
 /* 
 
@@ -31,16 +32,36 @@ as
 
 declare @sql as varchar(max)
 
-set @sql = 'select *
+set @sql = 'select OQ.*,
+
+case 
+	when (OQ.IMMD = ''M'' and OQ.IMMD2 = '' '') then (BOQty*OQ.IMFACT*imcost)
+	when (OQ.IMMD = ''M'' and OQ.IMMD2 = ''D'') then (((BOQty*imcost)*OQ.IMFACT)/OQ.IMFAC2)
+	else 0
+end as BOValue
+
 from openquery(gsfl2k,''
-select i1.imitem,
+select i1.imvend,
+i1.imfmcd,
+i1.imitem,
 i1.imdesc,
 i1.imcolr,
+i1.IMMD,
+i1.IMMD2,
+i1.imfact,
+i1.imfac2,
+i1.imcost,
 sum(SLECST+SLESC1+SLESC2+SLESC3+SLESC4+SLESC5) as SOCOGS,
-ifnull((select sum(PBOQTY*i2.IMCOST)
+
+ifnull((select sum(PBOQTY)
 		from poboline 
-		join itemmast i2 on i2.imitem = pbitem
-		where PBITEM = i1.imitem),0) as BOValue
+		join itemmast i3 on i3.imitem = pbitem
+		where PBITEM = i1.imitem),0) as BOQty,
+		
+ifnull((select sum(sleprc)
+		from shline sl 
+		where sl.slitem = i1.imitem
+		and year(SLDATE) = year(current_date)),0) as YTDSales
 
 from shline
 LEFT JOIN ITEMMAST i1 ON SHLINE.SLITEM = i1.IMITEM 
@@ -53,14 +74,24 @@ where SLDATE >= ''''' + @StartDate + '''''
  and i1.imfmcd not in (''''L2'''',''''W2'''')
  and i1.imdrop <> ''''D''''
 
-group by i1.imitem,
+group by i1.imvend,
+i1.imfmcd,
+i1.imitem,
 i1.imdesc,
-i1.imcolr
+i1.imcolr,
+i1.IMMD,
+i1.IMMD2,
+i1.imfact,
+i1.imfac2,
+i1.imcost
 
-'')'
+'') OQ;
 
+
+'
+
+--select (@sql)
 exec(@sql)
-
 
 
 GO
